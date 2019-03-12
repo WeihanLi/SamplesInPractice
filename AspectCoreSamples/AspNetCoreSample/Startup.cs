af7f400a4171cd65f20818865e914d1ca81c0b48
WeihanLi <weihanli@outlook.com>
2019-03-12 11:28:22 +0800

    add AspNetCoreSample and TotpSamples

---
+++ AspectCoreSamples/AspNetCoreSample/Startup.cs
@@ -0,0 +1,56 @@
+﻿using AspectCore.Configuration;
+using AspectCore.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AspNetCoreSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICustomService, CustomService>();
+            services.AddMvc();
+
+            services.AddDynamicProxy(config =>
+            {
+                config.Interceptors.AddTyped<LogInterceptorAttribute>();
+            });
+            return services.BuildAspectCoreServiceProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
