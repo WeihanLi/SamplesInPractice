10b0a3f468a87daccffb0cbf6732e361857033f7
weihanli <weihanli@outlook.com>
2020-04-13 18:52:56 +0800

    add AopSample.csproj

---
+++ AopSample/ProxyGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AopSample
+{
+    public interface IProxyGenerator
+    {
+        object CreateInterfaceProxy(Type serviceType);
+
+        object CreateInterfaceProxy(Type serviceType, object implementationInstance);
+
+        object CreateClassProxy(Type serviceType, Type implementationType, object[] args);
+    }
+
+    public class ProxyGenerator : IProxyGenerator
+    {
+        private static readonly ProxyGenerator _generator = new ProxyGenerator();
+
+        public static IProxyGenerator Instance => _generator;
+
+        public object CreateInterfaceProxy(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object CreateInterfaceProxy(Type serviceType, object implementationInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object CreateClassProxy(Type serviceType, Type implementationType, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AopSample/ProxyUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using WeihanLi.Extensions;
+
+namespace AopSample
+{
+    internal class ProxyUtil
+    {
+        private const string ProxyAssemblyName = "Aop.DynamicGenerated";
+        private readonly ModuleBuilder _moduleBuilder;
+        private readonly ConcurrentDictionary<string, Type> _proxyTypes = new ConcurrentDictionary<string, Type>();
+
+        public ProxyUtil()
+        {
+            var asmBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(ProxyAssemblyName), AssemblyBuilderAccess.RunAndCollect);
+            _moduleBuilder = asmBuilder.DefineDynamicModule("Default");
+        }
+
+        public Type CreateClassProxy(Type classType, params Type[] interfaceType)
+        {
+            var proxyTypeName = $"{ProxyAssemblyName}.{classType.FullName}.{(interfaceType ?? Enumerable.Empty<Type>()).OrderBy(t => t.FullName).StringJoin("_").Replace('.', '_')}";
+            var type = _proxyTypes.GetOrAdd(proxyTypeName, name =>
+            {
+                //
+                var typeBuilder = _moduleBuilder.DefineType(proxyTypeName, TypeAttributes.Public, classType);
+                //
+                var ctorBuilder = typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+
+                return classType;
+            });
+            return type;
+        }
+    }
+}
