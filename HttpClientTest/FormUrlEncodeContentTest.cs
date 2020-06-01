302749b35440949637f3433b86b79948d8c9e953
weihanli <weihanli@outlook.com>
2020-06-01 17:11:30 +0800

    add HttpClientTest.csproj

---
+++ HttpClientTest/FormUrlEncodeContentTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeihanLi.Common.Http;
+using WeihanLi.Extensions;
+
+namespace HttpClientTest
+{
+    public class FormUrlEncodeContentTest
+    {
+        private const string TestUrl = "https://cnblogs.com";
+
+        public static async Task FormUrlEncodedContentLengthTest()
+        {
+            using (var httpClient = new HttpClient(new NoProxyHttpClientHandler()))
+            {
+                using (var response = await httpClient.PostAsync(TestUrl, new FormUrlEncodedContent(new Dictionary<string, string>()
+                {
+                    {"bigContent", new string('a', 65535)},
+                })))
+                {
+                    Console.WriteLine($"response status code:{response.StatusCode}");
+                }
+            }
+        }
+
+        public static async Task ByteArrayContentLengthTest()
+        {
+            using (var httpClient = new HttpClient(new NoProxyHttpClientHandler()))
+            {
+                var postContent = $"bigContent={new string('a', 65535)}";
+                using (var response = await httpClient.PostAsync(TestUrl, new ByteArrayContent(postContent.GetBytes())))
+                {
+                    Console.WriteLine($"response status code:{response.StatusCode}");
+                }
+            }
+        }
+
+        public static async Task StringContentLengthTest()
+        {
+            using (var httpClient = new HttpClient(new NoProxyHttpClientHandler()))
+            {
+                var postContent = $"bigContent={new string('a', 65535)}";
+                using (var response = await httpClient.PostAsync(TestUrl, new StringContent(postContent)))
+                {
+                    Console.WriteLine($"response status code:{response.StatusCode}");
+                }
+            }
+        }
+    }
+}
---
+++ HttpClientTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeihanLi.Common.Helpers;
+
+namespace HttpClientTest
+{
+    public class Program
+    {
+        public static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+        public static async Task MainAsync(string[] args)
+        {
+            InvokeHelper.OnInvokeException = Console.WriteLine;
+
+            await InvokeHelper.TryInvokeAsync(FormUrlEncodeContentTest.FormUrlEncodedContentLengthTest);
+            Console.WriteLine();
+            await InvokeHelper.TryInvokeAsync(FormUrlEncodeContentTest.StringContentLengthTest);
+            Console.WriteLine();
+            await InvokeHelper.TryInvokeAsync(FormUrlEncodeContentTest.ByteArrayContentLengthTest);
+
+            Console.WriteLine("Completed!");
+            Console.ReadLine();
+        }
+    }
+}
