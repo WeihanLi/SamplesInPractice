90caf16b2b6ac523ef608fb6444c560f2a3da62a
weihanli <weihanli@outlook.com>
2020-06-24 15:51:50 +0800

    add NugetSample.csproj

---
+++ NugetSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NugetSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // RawApiSample.Test().Wait();
+            NugetClientSdkSample.Test().Wait();
+
+            Console.WriteLine("Completed...");
+            Console.ReadLine();
+        }
+    }
+}
