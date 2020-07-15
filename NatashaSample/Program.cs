b6909d38399a6b7d4aa475b10006c5ec1b17ec59
weihanli <weihanli@outlook.com>
2020-07-15 19:30:24 +0800

    add NatashaSample

---
+++ NatashaSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Natasha.CSharp;
+
+namespace NatashaSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var code = "Console.WriteLine(\"Hello World!\");";
+
+            AssemblyDomain.Init();
+            using var domain = DomainManagement.Random;
+            var action = NDelegate
+                .UseDomain(domain)
+                .UseRandomName()
+                .Action(code)
+                ;
+            action.Invoke();
+
+            Console.ReadLine();
+        }
+    }
+}
