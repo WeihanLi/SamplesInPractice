f5c1f998ee154ae79fca7b4e002a3295caed2ac1
weihanli <weihanli@outlook.com>
2020-12-28 23:32:30 +0800

    add CSharp9Sample.csproj

---
+++ CSharp9Sample/ForEachExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp9Sample
+{
+    public static class ForEachExtensions
+    {
+        private static IEnumerator<char> GetEnumerator(this int num)
+        {
+            return num.ToString().GetEnumerator();
+        }
+
+        public static void MainTest()
+        {
+            //var enumerable = Enumerable.Range(1, 10).ToArray();
+            //foreach (var i in enumerable)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //var enumerator = enumerable.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current);
+            //}
+
+            var num = 123;
+            foreach (var i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CSharp9Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp9Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ForEachExtensions.MainTest();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
