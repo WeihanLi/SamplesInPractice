61b9952740c9c068dba4c7d38c5cdbeaa3cd84b6
weihanli <weihanli@outlook.com>
2021-01-04 01:09:47 +0800

    update RecordSample

--- CSharp9Sample/RecordSample.cs
+++ CSharp9Sample/RecordSample.cs
@@ -9,6 +9,8 @@ namespace CSharp9Sample
             public string Name { get; init; }
 
             public int Age { get; init; }
+
+            // public RecordPerson2 Person2 { get; init; }
         }
 
         record RecordPerson2(string Name, int Age);
@@ -25,12 +27,20 @@ namespace CSharp9Sample
             var p2 = p1 with { Age = 10 };
             Console.WriteLine(p2);
 
+            var p2_1 = p2 with { };
+            Console.WriteLine($"Reference Equals:{ReferenceEquals(p2_1, p2)}, Equals:{p2_1.Equals(p2)}");
+
             var p3 = new RecordPerson() { Name = "Tom", Age = 12 };
             Console.WriteLine(p3);
             Console.WriteLine($"p1 Equals p3 =:{p1 == p3}");
 
             RecordPerson2 p4 = new("Tom", 12);
             Console.WriteLine(p4);
+
+            foreach (var (name, age) in new[] { p4 })
+            {
+                Console.WriteLine($"{name}={age}");
+            }
         }
     }
 }
