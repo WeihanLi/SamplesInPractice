b4b05e038569f60464fc3d99563498bf67cb445c
weihanli <weihanli@outlook.com>
2021-01-03 17:36:08 +0800

    update PatternMatchingSample

--- CSharp9Sample/PatternMatchingSample.cs
+++ CSharp9Sample/PatternMatchingSample.cs
@@ -24,6 +24,34 @@ namespace CSharp9Sample
             if (person.Name is not null)
             {
             }
+
+            // switch
+            switch (person.Age)
+            {
+                case >= 0 and <= 3:
+                    Console.WriteLine("baby");
+                    break;
+
+                case > 3 and < 14:
+                    Console.WriteLine("child");
+                    break;
+
+                case > 14 and < 22:
+                    Console.WriteLine("youth");
+                    break;
+
+                case > 22 and < 60:
+                    Console.WriteLine("Adult");
+                    break;
+
+                case >= 60 and <= 500:
+                    Console.WriteLine("Old man");
+                    break;
+
+                case > 500:
+                    Console.WriteLine("monster");
+                    break;
+            }
         }
     }
 }
