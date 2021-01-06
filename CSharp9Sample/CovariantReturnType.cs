15793125597d36f5052e437e3c30333851acda63
weihanli <weihanli@outlook.com>
2021-01-06 22:43:43 +0800

    add more sample

---
+++ CSharp9Sample/CovariantReturnType.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp9Sample
+{
+    internal class CovariantReturnType
+    {
+        private abstract class Operation
+        {
+        }
+
+        private abstract class OperationFactory
+        {
+            public abstract Operation GetOperation();
+        }
+
+        private class AddOperation : Operation
+        {
+        }
+
+        private class AddOperationFactory : OperationFactory
+        {
+            public override AddOperation GetOperation()
+            {
+                return new();
+            }
+        }
+
+        public static void MainTest()
+        {
+            var factory = new AddOperationFactory();
+            factory.GetOperation();
+        }
+    }
+}
---
+++ CSharp9Sample/StaticAnonymousMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CSharp9Sample
+{
+    internal class StaticAnonymousMethod
+    {
+        private readonly int num = 1;
+
+        public void MainTest()
+        {
+            // anonymous method
+            Action action = () => { Console.WriteLine(num); };
+            Action action1 = static () => { };
+
+            //expression
+            Expression<Func<int, bool>> expression = i => i > num;
+            Expression<Func<int, bool>> expression1 = static i => i > 1;
+        }
+    }
+}
