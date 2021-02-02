83eacb91289a7a597b1dd14af676e2c9fcd79e07
weihanli <weihanli@outlook.com>
2021-02-03 07:59:08 +0800

    add HttpClientTest/PollyIntegration.cs

---
+++ HttpClientTest/PollyIntegration.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+
+namespace HttpClientTest
+{
+    public class PollyIntegration
+    {
+        public static async Task MainTest()
+        {
+            var services = new ServiceCollection();
+            services.AddHttpClient("test")
+                .AddTransientHttpErrorPolicy(builder => builder.RetryAsync(5))
+                ;
+
+            await using var provider = services.BuildServiceProvider();
+            await provider.GetRequiredService<IHttpClientFactory>()
+                .CreateClient("test")
+                .GetAsync("");
+        }
+    }
+}
