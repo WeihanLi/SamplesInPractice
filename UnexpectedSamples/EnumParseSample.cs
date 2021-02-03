19488e6353addbf903f3be06c8250af1c022ee6b
weihanli <weihanli@outlook.com>
2021-02-03 23:43:58 +0800

    Add UnexpectedSamples.csproj

---
+++ UnexpectedSamples/EnumParseSample.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace UnexpectedSamples
+{
+    public class EnumParseSample
+    {
+        public static void MainTest()
+        {
+            Console.WriteLine(((Color)10).ToString());
+
+            var intValue = int.MaxValue;
+            Console.WriteLine(((Color)intValue).ToString());
+
+            if (Enum.TryParse("10", out Color color))
+            {
+                Console.WriteLine(color.ToString());
+            }
+            if (Enum.TryParse("10", out Color color1)
+                && Enum.IsDefined(typeof(Color), color1))
+            {
+                Console.WriteLine($"Success, {color1}");
+            }
+            else
+            {
+                Console.WriteLine("Can not match");
+            }
+
+            if (Enum.TryParse("Yellow", out Color color2))
+            {
+                Console.WriteLine(color2.ToString());
+            }
+            if (Enum.TryParse("Yellow", out Color color3)
+                && Enum.IsDefined(typeof(Color), color3))
+            {
+                Console.WriteLine($"Success, {color3}");
+            }
+            else
+            {
+                Console.WriteLine("Can not match");
+            }
+        }
+    }
+
+    public enum Color : byte
+    {
+        Red = 0,
+        Green = 1,
+        Blue = 2,
+    }
+}
---
+++ UnexpectedSamples/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnexpectedSamples;
+using WeihanLi.Common.Helpers;
+
+InvokeHelper.OnInvokeException = Console.WriteLine;
+InvokeHelper.TryInvoke(EnumParseSample.MainTest);
+
+Console.WriteLine("Hello World!");
+Console.ReadLine();
