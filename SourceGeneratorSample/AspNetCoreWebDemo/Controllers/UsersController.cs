7c30e74fa1ddb78179a3a714a90fbf8898d02087
weihanli <weihanli@outlook.com>
2021-05-13 23:32:05 +0800

    Add UsersController.cs

---
+++ SourceGeneratorSample/AspNetCoreWebDemo/Controllers/UsersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebDemo.Controllers
+{
+    public partial class UsersController
+    {
+        [HttpGet("test")]
+        public string Test() => "test";
+    }
+}
