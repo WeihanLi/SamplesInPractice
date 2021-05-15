5396ae2d8e3d021725839405f83746743f4e0335
weihanli <weihanli@outlook.com>
2021-05-15 22:03:56 +0800

    update SqlParserSample

--- SqlParserSample/Program.cs
+++ SqlParserSample/Program.cs
@@ -3,18 +3,18 @@ using System.Linq;
 using Microsoft.SqlServer.Management.SqlParser.Parser;
 using Microsoft.SqlServer.Management.SqlParser.SqlCodeDom;
 
-var sqlText = "SELECT TOP 100 * FROM dto.tabUsers WHERE Id > 10 ORDER BY Id DESC";
+var sqlText = "SELECT TOP 100 * FROM dbo.tabUsers WHERE Id > 10 ORDER BY Id DESC";
 var result = Parser.Parse(sqlText);
 Console.WriteLine(result.BatchCount);
 Console.WriteLine(result.Script.Sql);
 
 // var selectStatement = result.Script.Batches[0].Children.OfType<SqlSelectStatement>().First();
 // Console.WriteLine(selectStatement.Sql);
-//
+
 // Console.WriteLine(selectStatement.SelectSpecification.Sql);
 // Console.WriteLine(selectStatement.SelectSpecification.QueryExpression.Sql);
 // Console.WriteLine(selectStatement.SelectSpecification.OrderByClause?.Sql);
-//
+
 // foreach (var clause in selectStatement.SelectSpecification.Children)
 // {
 //     Console.WriteLine($"Sql:{clause.Sql}, Type:{clause.GetType().Name}");
@@ -23,10 +23,21 @@ Console.WriteLine(result.Script.Sql);
 Console.WriteLine("-------------------------------");
 IterateSqlNode(result.Script);
 
+Console.WriteLine("-------------------------------");
 sqlText = @"
-SELECT Id AS UserId, [Name] AS UserName, City AS [From] FROM dto.tabUsers
+SELECT Id AS UserId, [Name] AS UserName, City AS [From] FROM dbo.tabUsers
 WHERE Id>10
 ORDER BY Id DESC
+OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY
+";
+IterateSqlNode(Parser.Parse(sqlText).Script);
+
+Console.WriteLine("-------------------------------");
+sqlText = @"
+SELECT u.Id AS UserId, u.[Name] AS UserName, u.City AS [From] FROM dbo.tabUsers AS u WITH(NOLOCK)
+INNER JOIN dbo.tabUserRoles AS r WITH(NOLOCK) ON r.UserId= u.Id
+WHERE u.Id>10
+ORDER BY u.Id DESC
 OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY
 ";
 IterateSqlNode(Parser.Parse(sqlText).Script);
@@ -60,5 +71,5 @@ static string GetCountSql(string sql)
         throw new ArgumentException("Invalid query", nameof(sql));
     }
 
-    return sqlQuery.Sql.Replace(sqlQuery.SelectClause.Sql, "SELECT COUNT(1)");
+    return $@"SELECT COUNT(1) {sqlQuery.FromClause.Sql} {sqlQuery.WhereClause.Sql}";
 }
