62f2fd10420f0c941186f91aa5338ca9b06bb60d
weihanli <weihanli@outlook.com>
2021-05-16 11:57:00 +0800

    Add CSharp9Sample/DefaultInterfaceImplement

---
+++ CSharp9Sample/DefaultInterfaceImplement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using WeihanLi.Extensions;
+
+namespace CSharp9Sample
+{
+    internal interface IFlyH
+    {
+    }
+
+    internal interface IFly
+    {
+        private const string DefaultName = nameof(IFly);
+
+        protected static string GetDefaultName() => DefaultName;
+
+        public static string GetPublicName() => DefaultName;
+        
+        // Interface cannot contain instance fields
+        // private string name = "";
+        
+        string Name { get; }
+
+        void Fly() => Console.WriteLine($"{Name.GetValueOrDefault((DefaultName))} is flying");
+    }
+
+    internal class Superman : IFly
+    {
+        public string Name => nameof(Superman);
+    }
+
+    internal class MonkeyKing : IFly
+    {
+        public string Name => nameof(MonkeyKing);
+
+        public void Fly()
+        {
+            Console.WriteLine($"I'm {Name}, I'm flying, DefaultName:{IFly.GetDefaultName()}");
+        }
+    }
+
+    public class DefaultInterfaceImplement
+    {
+        public static void MainTest()
+        {
+            IFly fly = new Superman();
+            fly.Fly();
+
+            fly = new MonkeyKing();
+            fly.Fly();
+            
+            // Cannot access protected method 'GetDefaultName' here
+            // IFly.GetDefaultName().Dump();
+            
+            IFly.GetPublicName().Dump();
+        }
+    }
+}
