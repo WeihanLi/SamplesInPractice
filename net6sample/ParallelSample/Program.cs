a0def74f1857fabd5f7342cb97fc239e320a6d8a
weihanli <weihanli@outlook.com>
2021-06-11 10:39:41 +0800

    Add ParallelSample.csproj

---
+++ net6sample/ParallelSample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+var watch = Stopwatch.StartNew();
+await Task.WhenAll(Enumerable.Range(1, 100).Select(_ => Task.Delay(1000)));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
+watch.Restart();
+using var semaphore = new SemaphoreSlim(10, 10);
+await Task.WhenAll(Enumerable.Range(1, 100).Select(async _ =>
+{
+    try
+    {
+        await semaphore.WaitAsync();
+        await Task.Delay(1000);
+    }
+    finally
+    {
+        semaphore.Release();
+    }
+}));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
+Console.WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
+
+watch.Restart();
+await Parallel.ForEachAsync(Enumerable.Range(1, 100), async (_, _) => await Task.Delay(1000));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
+watch.Restart();
+await Parallel.ForEachAsync(Enumerable.Range(1, 100), new ParallelOptions()
+{
+    MaxDegreeOfParallelism = 10
+}, async (_, _) => await Task.Delay(1000));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
+watch.Restart();
+await Parallel.ForEachAsync(Enumerable.Range(1, 100), new ParallelOptions()
+{
+    MaxDegreeOfParallelism = 100
+}, async (_, _) => await Task.Delay(1000));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
+watch.Restart();
+await Parallel.ForEachAsync(Enumerable.Range(1, 100), new ParallelOptions()
+{
+    MaxDegreeOfParallelism = int.MaxValue
+}, async (_, _) => await Task.Delay(1000));
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
+WriteLine("Hello world");
