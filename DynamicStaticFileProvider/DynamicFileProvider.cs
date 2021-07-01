84c0bd6a4cbbcdca742ea01f7a428ca70fe1d808
weihanli <weihanli@outlook.com>
2021-07-01 22:56:23 +0800

    Add DynamicStaticFileProvider.csproj

---
+++ DynamicStaticFileProvider/DynamicFileProvider.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using System.IO;
+
+namespace DynamicStaticFileProvider
+{
+    public class DynamicFileProviderOptions
+    {
+        public string CurrentSlot { get; set; }
+    }
+
+    public class DynamicFileProvider : IFileProvider
+    {
+        private PhysicalFileProvider _physicalFileProvider;
+        private const string DefaultSlotName = "Slot1";
+
+        public DynamicFileProvider(IOptionsMonitor<DynamicFileProviderOptions> optionsMonitor, IWebHostEnvironment webHostEnvironment)
+        {
+            var webRoot = webHostEnvironment.ContentRootPath;
+            _physicalFileProvider = new PhysicalFileProvider(Path.Combine(webRoot, optionsMonitor.CurrentValue.CurrentSlot ?? DefaultSlotName));
+            optionsMonitor.OnChange(options =>
+            {
+                var path = Path.Combine(webRoot, options.CurrentSlot);
+                _physicalFileProvider = new PhysicalFileProvider(path);
+            });
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return _physicalFileProvider.GetDirectoryContents(subpath);
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            return _physicalFileProvider.GetFileInfo(subpath);
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            return _physicalFileProvider.Watch(filter);
+        }
+    }
+}
