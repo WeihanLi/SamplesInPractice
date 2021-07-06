3bcdef2670083d580353b155f2bc82495c4f6e9a
weihanli <weihanli@outlook.com>
2021-07-06 23:30:10 +0800

    Add RequestFilteringExtensionSample

---
+++ BlazorSample/RequestFilteringExtensionSample/Pages/Background.razor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Extensions.Logging;
+using WebExtensions.Net.WebRequest;
+using WeihanLi.Extensions;
+
+namespace RequestFilteringExtensionSample.Pages
+{
+    public partial class Background
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            await WebExtensions.WebRequest.OnBeforeRequest.AddListener(info =>
+            {
+                var targetUrl = GetTargetUrl(info.Url);
+                if (info.Url.Equals(targetUrl)) return new BlockingResponse();
+                return new BlockingResponse {RedirectUrl = targetUrl};
+            }, new RequestFilter
+            {
+                Urls = new[] {"*://docs.microsoft.com/*"}, 
+                Types = new[] {ResourceType.MainFrame}
+            }, new[] {OnBeforeRequestOptions.Blocking});
+        }
+
+        private const string TrackQueryKey = "WT.mc_id";
+        private const string TrackQueryValue = "DT-MVP-5004222";
+
+        private string GetTargetUrl(string originalUrl)
+        {
+            if (!Uri.TryCreate(originalUrl, UriKind.Absolute, out var uri))
+            {
+                return originalUrl;
+            }
+            try
+            {
+                var queryString = HttpUtility.ParseQueryString(uri.Query);
+                if (TrackQueryValue == queryString.Get(TrackQueryKey))
+                {
+                    return originalUrl;
+                }
+                queryString.Set(TrackQueryKey, TrackQueryValue);
+                var fullPath = $"{uri.Scheme}://{uri.Host}{uri.LocalPath}";
+                var targetUrl =
+                    $"{fullPath}?{queryString.AllKeys.Where(k => k.IsNotNullOrWhiteSpace()).Select(k => $"{k}={HttpUtility.UrlEncode(queryString[k])}").StringJoin("&")}";
+                Logger.LogInformation($"original url: {originalUrl}, new url: {targetUrl}");
+                return targetUrl;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, $"Exception in {nameof(GetTargetUrl)}, {nameof(originalUrl)}:{originalUrl}");
+                return originalUrl;
+            }
+        }
+    }
+}
---
+++ BlazorSample/RequestFilteringExtensionSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RequestFilteringExtensionSample
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            // workaround to use JavaScript fetch to bypass url validation
+            // see: https://github.com/dotnet/runtime/issues/52836
+            builder.Services.AddScoped<HttpClient>(sp => new JsHttpClient(sp) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBrowserExtensionServices(options =>
+            {
+                options.ProjectNamespace = typeof(Program).Namespace;
+            });
+            await builder.Build().RunAsync();
+        }
+    }
+}
