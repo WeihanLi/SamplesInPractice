0eacf6b3d5ba4fee3f22046724a1130e65b412ca
weihanli <weihanli@outlook.com>
2021-08-22 00:50:13 +0800

    Add CommandLineAppSamples/GetStarted/GetStarted.csproj

---
+++ CommandLineAppSamples/GetStarted/Program.cs
@@ -0,0 +1,14 @@
+﻿var command = new RootCommand("h");
+command.AddOption(new Option(new[] { "-v", "--verbose" }, "Verbose"));
+command.AddOption(new Option("--schema", "schema"));
+
+var parseResult = command.Parse(args);
+
+Console.WriteLine("----- Tokens ------");
+foreach (var token in parseResult.Tokens)
+{
+    Console.WriteLine($"{token.Type}: {token.Value}");
+}
+Console.WriteLine("------------------");
+
+Console.WriteLine($"Schema value: {parseResult.ValueForOption("--schema")}");
