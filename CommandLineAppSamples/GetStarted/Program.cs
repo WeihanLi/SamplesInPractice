d6c046dc67234d7422d509e2acbfabae16847133
weihanli <weihanli@outlook.com>
2021-08-22 14:48:49 +0800

    Update CommandLineAppSamples/GetStarted

--- CommandLineAppSamples/GetStarted/Program.cs
+++ CommandLineAppSamples/GetStarted/Program.cs
@@ -1,5 +1,12 @@
 ﻿var command = new RootCommand("h");
-var schemaOption = new Option<string>("--schema", "schema");
+var methodArgument = new Argument<HttpMethod>("method", ()=> HttpMethod.Get, "Request method");
+methodArgument.Arity = ArgumentArity.ZeroOrOne;
+command.AddArgument(methodArgument);
+var urlArgument = new Argument<string>("url", "Request url");
+urlArgument.Arity = ArgumentArity.ExactlyOne;
+command.AddArgument(urlArgument);
+
+var schemaOption = new Option<string>("--schema", ()=> "http", "schema");
 command.AddOption(new Option(new[] { "-v", "--verbose" }, "Verbose"));
 command.AddOption(schemaOption);
 
@@ -13,3 +20,4 @@ foreach (var token in parseResult.Tokens)
 Console.WriteLine("------------------");
 
 Console.WriteLine($"Schema value: {parseResult.ValueForOption(schemaOption)}");
+Console.WriteLine($"Request：{parseResult.ValueForArgument(methodArgument)} {parseResult.ValueForArgument(urlArgument)}");
