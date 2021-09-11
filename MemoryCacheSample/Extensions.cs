5dd2f64ad4d1e396082f8b858e43d70a55e670ba
weihanli <weihanli@outlook.com>
2021-09-11 13:49:17 +0800

    Add MemoryCacheSample.csproj

---
+++ MemoryCacheSample/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System;
+
+static class Extensions
+{
+    public static void Dump(this List<int> values)
+    {
+        var value = string.Join(",", values);
+        Console.WriteLine(value);
+    }
+}
---
+++ MemoryCacheSample/Program.cs
@@ -0,0 +1,32 @@
+﻿await using var services = new ServiceCollection()
+    .AddMemoryCache()
+    .BuildServiceProvider();
+
+Console.WriteLine("----- Bad -----");
+GetValidValues_Bad(5).Dump();
+GetValidValues_Bad(8).Dump();
+
+Console.WriteLine("----- Good -----");
+GetValidValues_Good(5).Dump();
+GetValidValues_Good(8).Dump();
+
+List<int> GetValidValues_Bad(int threhold)
+{
+    var memoryCache = services.GetRequiredService<IMemoryCache>();
+    var values = memoryCache.GetOrCreate("test1", entry =>
+    {
+        return Enumerable.Range(1, 10).ToList();
+    });
+    values.RemoveAll(x => x > threhold);
+    return values;
+}
+
+List<int> GetValidValues_Good(int threhold)
+{
+    var memoryCache = services.GetRequiredService<IMemoryCache>();
+    var values = memoryCache.GetOrCreate("test", entry =>
+    {
+        return Enumerable.Range(1, 10).ToList();
+    });
+    return values.Where(v => v <= threhold).ToList();
+}
