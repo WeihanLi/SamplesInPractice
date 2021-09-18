640c79f3ac0b87827aa490bb39e49bcaa800899d
weihanli <weihanli@outlook.com>
2021-09-18 22:23:40 +0800

    Add EF6Samples.csproj

---
+++ EF6Samples/Program.cs
@@ -0,0 +1,22 @@
+﻿const string connectionString = "DataSource=test";
+
+var services = new ServiceCollection();
+
+// services.AddDbContext<TestDbContext>(options => options.UseSqlite(connectionString));
+services.AddSqlite<TestDbContext>(connectionString);
+
+using var serviceProvider = services.BuildServiceProvider();
+
+using var scope = serviceProvider.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<TestDbContext>();
+dbContext.Database.EnsureDeleted();
+dbContext.Database.EnsureCreated();
+dbContext.Users.Add(new User 
+{ 
+    Name = "Alice",
+    CreatedAt = DateTime.UtcNow
+});
+await dbContext.SaveChangesAsync();
+
+var users = await dbContext.Users.AsNoTracking().ToArrayAsync();
+users.Dump();
---
+++ EF6Samples/TestDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace EF6Samples;
+
+public class TestDbContext : DbContext
+{
+    public TestDbContext(DbContextOptions<TestDbContext> options):base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+}
+
+public record User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public DateTime CreatedAt { get; init; }
+}
