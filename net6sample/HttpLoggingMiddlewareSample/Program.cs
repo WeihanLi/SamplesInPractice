4dbb5c5005b4ecf9e4e850309d4c82c7cf7b47e4
weihanli <weihanli@outlook.com>
2021-09-24 22:21:10 +0800

    Add HttpLoggingMiddlewareSample.csproj

---
+++ net6sample/HttpLoggingMiddlewareSample/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpLogging(options =>
+{
+    // options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+    options.RequestHeaders.Add("Cache-Control");
+    options.ResponseHeaders.Add("Server");
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpLogging();
+app.MapControllers();
+
+app.Run();
