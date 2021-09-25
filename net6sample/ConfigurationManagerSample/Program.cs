46d216fc3dcaff9c527af2e3291010602cd17f1d
weihanli <weihanli@outlook.com>
2021-09-25 19:00:53 +0800

    Add ConfigurationManagerSample.csproj

---
+++ net6sample/ConfigurationManagerSample/Program.cs
@@ -0,0 +1,11 @@
+﻿const string testKey = "test";
+
+var configuration = new ConfigurationManager();
+Console.WriteLine(configuration[testKey]);
+
+configuration.AddInMemoryCollection(new Dictionary<string, string>() 
+{
+    { testKey, "test" }
+});
+Console.WriteLine(configuration[testKey]);
+Console.ReadLine();
