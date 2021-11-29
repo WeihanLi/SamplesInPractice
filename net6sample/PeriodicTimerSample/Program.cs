a4e617ce89b4548ff706d85055a9365a943c5a57
weihanli <weihanli@outlook.com>
2021-11-29 22:20:12 +0800

    Add PeriodicTimerSample.csproj

---
+++ net6sample/PeriodicTimerSample/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(3));
+try
+{
+    while (await timer.WaitForNextTickAsync(cts.Token))
+    {
+        Console.WriteLine($"Timed event triggered({DateTime.Now:HH:mm:ss})");
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Operation cancelled");
+}
+Console.WriteLine("Hello world");
