f8f8bed438408e0d4761d472caff77fe8bf24a4f
weihanli <weihanli@outlook.com>
2021-12-13 23:08:43 +0800

    Update GeneratorSample/Program.cs

--- net6sample/GeneratorSample/Program.cs
+++ net6sample/GeneratorSample/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// LoggingGeneratorSample.MainTest();
+LoggingGeneratorSample.MainTest();
 JsonGeneratorSample.MainTest();
 
 Console.WriteLine("Hello, World!");
