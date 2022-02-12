37c382812677c9c9ecac8c16fd256ec0c6d8b58a
Weihan Li <weihanli@outlook.com>
2022-02-13 01:12:55 +0800

    Update AzureSamples/SpeechSample

--- AzureSamples/SpeechSample/Program.cs
+++ AzureSamples/SpeechSample/Program.cs
@@ -11,6 +11,7 @@ var key = Environment.GetEnvironmentVariable("SpeechSubscriptionKey");
 var config = SpeechConfig.FromSubscription(key, "eastasia");
 //https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support
 config.SpeechSynthesisLanguage = locale;
+config.OutputFormat = OutputFormat.Detailed;
 
 // Creates a speech synthesizer
 using var synthesizer = new SpeechSynthesizer(config);
@@ -33,13 +34,33 @@ foreach (var voice in voices)
     }
 
     config.SpeechRecognitionLanguage = locale;
+    config.RequestWordLevelTimestamps();
+
     using (var input = AudioConfig.FromWavFileInput(outputFileName))
     {
         using var speechRecognizer = new SpeechRecognizer(config, autoDetectSourceLanguageConfig, input);
         var speechRecognizeResult = await speechRecognizer.RecognizeOnceAsync();
         Console.WriteLine(speechRecognizeResult.Reason);
-        Console.WriteLine(speechRecognizeResult.Text);
+        Console.WriteLine($"RECOGNIZED: Text = {speechRecognizeResult.Text}");
+
+        Console.WriteLine(" Detailed results:");
+        var detailedResult = speechRecognizeResult.Best();
+        foreach (var item in detailedResult)
+        {
+            Console.WriteLine($"\tConfidence: {item.Confidence}\n\tText: {item.Text}\n\tLexicalForm: {item.LexicalForm}\n\tNormalizedForm: {item.NormalizedForm}\n\tMaskedNormalizedForm: {item.MaskedNormalizedForm}");
+            Console.WriteLine($"\tWord-level timing:");
+            Console.WriteLine($"\t\tWord | Offset | Duration");
+
+            // Word-level timing
+            foreach (var word in item.Words)
+            {
+                Console.WriteLine($"\t\t{word.Word} {word.Offset} {word.Duration}");
+            }
+        }
     }
+
+    Console.WriteLine("Press 'Enter' to continue");
+    Console.ReadLine();
 }
 
 Console.WriteLine("Completed");
