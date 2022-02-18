21acb9fd60d47a5b865adae03e0821da8562469b
Weihan Li <weihanli@outlook.com>
2022-02-19 00:34:50 +0800

    Add CSharp11Sample.csproj

---
+++ CSharp11Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿Hello("World");
+
+try
+{
+    Hello(null!);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+void Hello(string name!!)
+{
+    Console.WriteLine($"Hello, {name}!");
+}
