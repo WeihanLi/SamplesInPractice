5314bacd9dec9240bcab4cf216cbca445e8b591a
Weihan Li <weihanli@outlook.com>
2022-02-21 22:44:37 +0800

    Add DockerSample/AspNetCoreSample

---
+++ DockerSample/AspNetCoreSample/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AspNetCoreSample.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok(Dns.GetHostAddresses(Dns.GetHostName())
+            .Where(x => x.AddressFamily == AddressFamily.InterNetwork)
+            .Select(ip => ip.ToString())
+            .ToArray());
+    }
+
+    [HttpGet("test")]
+    public async Task<IActionResult> Test(string ip, [FromServices] HttpClient httpClient)
+    {
+        var url = $"http://{ip}/ap/values";
+        try
+        {
+            var result = await httpClient.GetFromJsonAsync<string[]>(url);
+            return Ok(new { url, result });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { url, Error = e.Message });
+        }
+    }
+}
---
+++ DockerSample/AspNetCoreSample/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSingleton(sp => new HttpClient());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
