3a8861629a5020ea043dc326f6f7e0bd1b5fbeab
Weihan Li <weihanli@outlook.com>
2022-02-23 23:42:22 +0800

    Add HttpClientTest/JsonExtensionSample.cs

---
+++ HttpClientTest/JsonExtensionSample.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using WeihanLi.Common.Models;
+
+namespace HttpClientTest;
+
+public class JsonExtensionSample
+{
+    public static async Task MainTest()
+    {
+        const string url = "https://reservation.weihanli.xyz/health";
+        using var httpClient = new HttpClient();
+
+        {
+            using var response = await httpClient.PostAsJsonAsync(url, new Category()
+            {
+                Id = 1,
+                Name = "Test"
+            });
+            response.EnsureSuccessStatusCode();
+        }
+
+        {
+            using var response = await httpClient.PostAsJsonAsync(url, new Category()
+            {
+                Id = 1,
+                Name = "Test"
+            }, new JsonSerializerOptions());
+            response.EnsureSuccessStatusCode();
+        }
+
+        {
+            using var response = await httpClient.PutAsJsonAsync(url, new Category()
+            {
+                Id = 1,
+                Name = "Test"
+            });
+            response.EnsureSuccessStatusCode();
+        }
+
+        {
+            using var content = JsonContent.Create(new Category() { Id = 1, Name = "Test" });
+            using var response = await httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+
+            var result = await content.ReadFromJsonAsync<Category>();
+            ArgumentNullException.ThrowIfNull(result);
+            Console.WriteLine($"{result.Id}: {result.Name}");
+        }
+
+        {
+            var result = await httpClient.GetFromJsonAsync<ResultModel>(url);
+            ArgumentNullException.ThrowIfNull(result);
+            Console.WriteLine($"{result.Status}: {result.ErrorMsg}");
+        }
+
+        {
+            var result = await httpClient.GetFromJsonAsync<ResultModel>(url, new JsonSerializerOptions());
+            ArgumentNullException.ThrowIfNull(result);
+            Console.WriteLine($"{result.Status}: {result.ErrorMsg}");
+        }
+    }
+}
