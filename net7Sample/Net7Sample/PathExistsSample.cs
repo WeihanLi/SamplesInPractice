0dbed94a9be23f06cea03ce097ff083fdaf35731
Weihan Li <weihanli@outlook.com>
2022-05-11 14:03:44 +0800

    Add PathExistsSample

---
+++ net7Sample/Net7Sample/PathExistsSample.cs
@@ -0,0 +1,18 @@
+﻿namespace Net7Sample;
+
+public class PathExistsSample
+{
+    public static void MainTest()
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        Console.WriteLine(Path.Exists(currentDir));
+
+        var files = Directory.GetFiles(currentDir);
+        if (files.Length > 0)
+        {
+            var fileName = files[0];
+            Console.WriteLine(File.Exists(fileName));
+            Console.WriteLine(Path.Exists(fileName));
+        }
+    }
+}
--- net7Sample/Net7Sample/Program.cs
+++ net7Sample/Net7Sample/Program.cs
@@ -1,4 +1,5 @@
 ﻿// await InvokeHelper.TryInvokeAsync(HttpClientJsonSample.MainTest);
-await InvokeHelper.TryInvokeAsync(HostApplicationBuilderSample.MainTest);
-
+// await InvokeHelper.TryInvokeAsync(HostApplicationBuilderSample.MainTest);
+await ServiceCollectionSample.MainTest();
+PathExistsSample.MainTest();
 Console.ReadLine();
