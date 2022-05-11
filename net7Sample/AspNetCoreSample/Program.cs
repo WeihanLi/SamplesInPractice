6dbba3e63fa06d88f6cac2edc826653dc7fbaf06
Weihan Li <weihanli@outlook.com>
2022-05-11 14:20:00 +0800

    Add net7Sample/AspNetCoreSample

---
+++ net7Sample/AspNetCoreSample/Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Map("/", () => "Hello MinimalAPI").AddFilter<OutputDotNetVersionFilter>();
+
+app.MapControllers();
+
+app.Run();
+
+internal sealed class OutputDotNetVersionFilter : IRouteHandlerFilter
+{
+    public async ValueTask<object?> InvokeAsync(RouteHandlerInvocationContext context, RouteHandlerFilterDelegate next)
+    {
+        context.HttpContext.Response.Headers["X-NET-Version"] = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+        return await next(context);
+    }
+}
