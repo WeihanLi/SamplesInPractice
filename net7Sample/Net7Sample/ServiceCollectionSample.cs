0614df20e003da9155dd815553e8ac4e8e3fc2b0
Weihan Li <weihanli@outlook.com>
2022-05-11 14:00:32 +0800

    Add ServiceCollectionSample

---
+++ net7Sample/Net7Sample/ServiceCollectionSample.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeihanLi.Common.Http;
+using WeihanLi.Common.Services;
+
+namespace Net7Sample;
+
+public class ServiceCollectionSample
+{
+    public static async Task MainTest()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<IUserIdProvider, EnvironmentUserIdProvider>();
+        await using (var provider = services.BuildServiceProvider())
+        {
+            Console.WriteLine(provider.GetRequiredService<IUserIdProvider>().GetHashCode());
+        }
+
+        await using (var provider2 = services.BuildServiceProvider())
+        {
+            Console.WriteLine(provider2.GetRequiredService<IUserIdProvider>().GetHashCode());
+        }
+
+        //
+        Console.WriteLine(services.IsReadOnly);
+
+        // https://github.com/dotnet/runtime/pull/68051
+        services.MakeReadOnly();
+
+        Console.WriteLine(services.IsReadOnly);
+
+        try
+        {
+            services.AddSingleton<IHttpRequester, HttpClientHttpRequester>();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
