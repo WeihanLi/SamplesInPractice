6716e5ee7941421dd794a4400085445d8eeb0ffc
Weihan Li <weihanli@outlook.com>
2022-07-23 10:34:34 +0800

    enhancements for images

--- YarpSamples/GithubProxySample/Program.cs
+++ YarpSamples/GithubProxySample/Program.cs
@@ -1,21 +1,31 @@
+﻿using System.Text.RegularExpressions;
 using Yarp.ReverseProxy.Transforms;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
-    .AddTransforms((context) =>
+    .AddTransforms(context =>
     {
-        if (context.Route.ClusterId == "githubimages")
+        context.AddRequestTransform(transformContext =>
         {
-            context.AddRequestTransform(transformContext =>
+            const string imageRegex = @".+\.(jpg|png|gif|jpeg)$";
+            var requestPath = transformContext.HttpContext.Request.Path.Value ?? "/";
+            if (context.Cluster?.ClusterId != "githubImages" &&
+                !Regex.IsMatch(requestPath, imageRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase))
             {
-                var path = transformContext.HttpContext.Request.Path.Value?.Replace("/blob/", "/") ?? "/";
-                var url = "https://raw.githubusercontent.com" + path + transformContext.HttpContext.Request.QueryString;
-                transformContext.ProxyRequest.RequestUri = new Uri(url);
                 return default;
-            });
-        }
+            }
+            var path = transformContext.HttpContext.Request.Path.Value
+                           ?.Replace("/blob/", "/").Replace("/raw/", "/")
+                       ?? "/";
+            var query = transformContext.HttpContext.Request.QueryString.HasValue
+                ? $"?{transformContext.HttpContext.Request.QueryString}"
+                : "";
+            var url = $"https://raw.githubusercontent.com{path}{query}";
+            transformContext.ProxyRequest.RequestUri = new Uri(url);
+            return default;
+        });
     });
 
 var app = builder.Build();
