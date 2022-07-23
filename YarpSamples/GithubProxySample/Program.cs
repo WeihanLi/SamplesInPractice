b8e857f11bc24e23fd41ab2d7015b7bdf6bea6b7
Weihan Li <weihanli@outlook.com>
2022-07-23 11:22:14 +0800

    Update GithubProxySample

--- YarpSamples/GithubProxySample/Program.cs
+++ YarpSamples/GithubProxySample/Program.cs
@@ -7,25 +7,21 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
     .AddTransforms(context =>
     {
-        context.AddRequestTransform(transformContext =>
+        if (context.Cluster?.ClusterId == "githubImages")
         {
-            const string imageRegex = @".+\.(jpg|png|gif|jpeg)$";
-            var requestPath = transformContext.HttpContext.Request.Path.Value ?? "/";
-            if (context.Cluster?.ClusterId != "githubImages" &&
-                !Regex.IsMatch(requestPath, imageRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+            context.AddRequestTransform(transformContext =>
             {
+                var path = transformContext.HttpContext.Request.Path.Value
+                            ?.Replace("/blob/", "/").Replace("/raw/", "/")
+                        ?? "/";
+                var query = transformContext.HttpContext.Request.QueryString.HasValue
+                    ? $"?{transformContext.HttpContext.Request.QueryString}"
+                    : "";
+                var url = $"https://raw.githubusercontent.com{path}{query}";
+                transformContext.ProxyRequest.RequestUri = new Uri(url);
                 return default;
-            }
-            var path = transformContext.HttpContext.Request.Path.Value
-                           ?.Replace("/blob/", "/").Replace("/raw/", "/")
-                       ?? "/";
-            var query = transformContext.HttpContext.Request.QueryString.HasValue
-                ? $"?{transformContext.HttpContext.Request.QueryString}"
-                : "";
-            var url = $"https://raw.githubusercontent.com{path}{query}";
-            transformContext.ProxyRequest.RequestUri = new Uri(url);
-            return default;
-        });
+            });
+        }
     });
 
 var app = builder.Build();
