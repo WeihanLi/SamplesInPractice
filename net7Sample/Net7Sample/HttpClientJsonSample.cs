b128b60ebdeb35448c9b9eb0ec65f0c2e00cd803
Weihan Li <weihanli@outlook.com>
2022-09-28 21:07:39 +0800

    Add HttpClientJsonSample DeleteFromJsonAsync

--- net7Sample/Net7Sample/HttpClientJsonSample.cs
+++ net7Sample/Net7Sample/HttpClientJsonSample.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using WeihanLi.Common.Models;
 
 namespace Net7Sample;
 public class HttpClientJsonSample
@@ -6,12 +7,17 @@ public class HttpClientJsonSample
     public static async Task MainTest()
     {
         using var httpClient = new HttpClient();
-        using var response = await httpClient.PatchAsJsonAsync("https://reservation.weihanli.xyz/health", new 
+        var url = "https://reservation.weihanli.xyz/health";
+
+        using var response = await httpClient.PatchAsJsonAsync(url, new 
         { 
             Name = "test",
             Age = 10
         });
         response.EnsureSuccessStatusCode();
         Console.WriteLine(response.StatusCode.ToString());
+
+        var result = await httpClient.DeleteFromJsonAsync<Result>(url);
+        System.Console.WriteLine(result);
     }
 }
