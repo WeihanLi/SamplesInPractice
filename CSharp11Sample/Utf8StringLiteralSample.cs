41afad7cd2953f91d2845d41a7a6fa46b1f8aaec
Weihan Li <weihanli@outlook.com>
2022-11-25 00:21:20 +0800

    Update Utf8StringLiteralSample.cs

--- CSharp11Sample/Utf8StringLiteralSample.cs
+++ CSharp11Sample/Utf8StringLiteralSample.cs
@@ -5,9 +5,11 @@ public class Utf8StringLiteralSample
     public static void MainTest()
     {
         var helloBytes = "Hello"u8;
+        Console.WriteLine(helloBytes.GetType().FullName);
         Console.WriteLine(Encoding.UTF8.GetString(helloBytes));
 
         var authStringLiteral = "AUTH "u8.ToArray();
+        Console.WriteLine(authStringLiteral.GetType().FullName);
         Console.WriteLine(Encoding.UTF8.GetString(authStringLiteral));
     }
 }
