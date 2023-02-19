68c5ccd59048cf63a01b467d298a9ed06540481d
Weihan Li <weihanli@outlook.com>
2023-02-19 11:10:54 +0800

    Add FluentAssertionsTest.cs

---
+++ XunitSample/FluentAssertionsTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace XunitSample;
+
+public class FluentAssertionsTest
+{
+    [Fact]
+    public void AddTest()
+    {
+        Helper.Add(2, 2).Should().Be(4);
+        Helper.Add(2, 2).Should().NotBe(3, "Just a sample");
+        
+        Helper.Add(2, 2).Should().BePositive();
+    }
+    
+    [Fact]
+    public void StringTest()
+    {
+        "Hello World".Should().NotBeEmpty();
+        "Hello World".Should().NotBeNullOrEmpty();
+        "Hello World".Should().StartWith("Hello ");
+        "Hello World".Should().Be("Hello World");
+    }
+}
