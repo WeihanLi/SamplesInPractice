be58235ff895ff123271c9186968007ea360970d
Weihan Li <weihanli@outlook.com>
2023-02-21 23:46:15 +0800

    Update XunitSample

--- XunitSample/FluentAssertionsTest.cs
+++ XunitSample/FluentAssertionsTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
 using System;
 using Xunit;
 
@@ -57,4 +58,54 @@ public class FluentAssertionsTest
             action2.Should().ThrowExactly<ArgumentNullException>();
         }
     }
+    
+    [Fact]
+    public void DateTest()
+    {
+        var date = DateOnly.FromDateTime(DateTime.Today);
+        date.Should().HaveYear(DateTime.Now.Year);
+        date.Should().HaveMonth(DateTime.Now.Month);
+        date.Should().HaveDay(DateTime.Now.Day);
+    }
+
+    [Fact]
+    public void ExtensionTest()
+    {
+        var model = new TestModel() { Name = "Hello" };
+        model.Should().WithName("Hello");
+        model.Should().WithNamePrefix("He").And.WithName("Hello");
+    }
+}
+
+file sealed class TestModel
+{
+    public required string Name { get; init; }
+}
+
+file sealed class TestModelAssertions : ReferenceTypeAssertions<TestModel, TestModelAssertions>
+{
+    public TestModelAssertions(TestModel subject) : base(subject)
+    {
+    }
+
+    protected override string Identifier { get; } = "Name";
+
+    public AndConstraint<TestModelAssertions> WithName(string name, string because = "", params object[] becauseArgs)
+    {
+        Subject.Name.Should().Be(name, because, becauseArgs);
+        return new AndConstraint<TestModelAssertions>(this);
+    }
+    
+    public AndConstraint<TestModelAssertions> WithNamePrefix(string prefix, string because = "", params object[] becauseArgs)
+    {
+        Execute.Assertion.BecauseOf(because, becauseArgs)
+            .ForCondition(Subject.Name.StartsWith(prefix))
+            .FailWith("Name should had prefix {0}", prefix);
+        return new AndConstraint<TestModelAssertions>(this);
+    }
+}
+
+file static class TestModelAssertionsExtensions
+{
+    public static TestModelAssertions Should(this TestModel testModel) => new TestModelAssertions(testModel);
 }
