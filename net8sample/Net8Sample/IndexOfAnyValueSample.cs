abfad5457f2e65eb9996084ae79fa688a1351594
Weihan Li <weihanli@outlook.com>
2023-02-28 22:54:30 +0800

    Add IndexOfAnyValueSample.cs

---
+++ net8sample/Net8Sample/IndexOfAnyValueSample.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers;
+
+namespace Net8Sample;
+
+public class IndexOfAnyValueSample
+{
+    public static void MainTest()
+    {
+        // ReSharper disable StringLiteralTypo
+        var chars = "-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        var i = "Hello".IndexOfAny(chars);
+        Console.WriteLine(i);
+        
+        var chars2 = IndexOfAnyValues.Create("-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz");
+        var i2 = "Hello".AsSpan().IndexOfAny(chars2);
+        Console.WriteLine(i2);
+    }
+}
