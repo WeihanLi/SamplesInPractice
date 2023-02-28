e46936d7ad23257d802274d05c8b37c0d0faece1
Weihan Li <weihanli@outlook.com>
2023-02-28 22:54:18 +0800

    Add FrozenCollectionSample.cs

---
+++ net8sample/Net8Sample/FrozenCollectionSample.cs
@@ -0,0 +1,23 @@
+﻿using System.Buffers;
+using System.Collections.Frozen;
+
+namespace Net8Sample;
+
+public static class FrozenCollectionSample
+{
+    public static void MainTest()
+    {
+        var elements = new[] { "Hello", "hello", "World" };
+        var set = elements.ToFrozenSet(StringComparer.OrdinalIgnoreCase);
+        Console.WriteLine(set.Contains("Hello"));
+
+        Console.WriteLine(set.IsSubsetOf(new[] { "Hello", "World" }));
+        Console.WriteLine(set.IsSupersetOf(new[] { "Hello" }));
+        Console.WriteLine(set.IsProperSupersetOf(new[] { "Hello" }));
+        Console.WriteLine(set.IsProperSubsetOf(new[] { "Hello", "World", "Test" }));
+
+        var dic = elements.ToFrozenDictionary(x => x, x => x[..1]);
+        var val = dic.GetValueRefOrNullRef("hello");
+        Console.WriteLine(val);
+    }
+}
