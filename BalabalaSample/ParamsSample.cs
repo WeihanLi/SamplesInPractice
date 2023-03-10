69a3ee7a91dfbfe47a2c75f27c5958abf2f2c2ee
Weihan Li <weihanli@outlook.com>
2023-03-10 22:45:41 +0800

    Add ParamsSample

---
+++ BalabalaSample/ParamsSample.cs
@@ -0,0 +1,65 @@
+﻿using WeihanLi.Common.Helpers;
+
+namespace BalabalaSample;
+
+public class ParamsSample
+{
+    public static void MainTest()
+    {
+        ParamsTest();
+        Console.ReadLine();
+        StringFormatTest();
+    }
+
+    private static void StringFormatTest()
+    {
+        var format = "{0} 123 {1}";
+        InvokeHelper.TryInvoke(() =>
+        {
+            var str = string.Format(format, 1, 2);
+            Console.WriteLine(str);
+        });
+
+        InvokeHelper.TryInvoke(() =>
+        {
+            var str = string.Format(format, new[]{1, 2});
+            Console.WriteLine(str);
+        });
+        
+        InvokeHelper.TryInvoke(() =>
+        {
+            var str = string.Format(format,  new []{"1", "2"});
+            Console.WriteLine(str); 
+        });
+        
+        InvokeHelper.TryInvoke(() =>
+        {
+            var str = string.Format(format,  new []{1, 2}.Cast<object>().ToArray());
+            Console.WriteLine(str); 
+        });
+        
+        InvokeHelper.TryInvoke(() =>
+        {
+            var str = string.Format(format, new object[]{1, 2});
+            Console.WriteLine(str); 
+        });
+    }
+
+    private static void ParamsTest()
+    {
+        ParamsMethod(1, 2, 3);
+        ParamsMethod(new[] { 1, 2, 3 });
+        ParamsMethod(new[]{"1","2","3"});
+        ParamsMethod(new object[]{1, 2, 3});
+    }
+
+    private static void ParamsMethod(params object[] args)
+    {
+        if (args is null)
+        {
+            Console.WriteLine("Empty");
+            return;
+        }
+        Console.WriteLine(args.Length);
+    }
+}
