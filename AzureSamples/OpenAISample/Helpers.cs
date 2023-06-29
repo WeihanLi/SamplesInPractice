3010fe5caf778ac01d892177b191ba89bef8df85
Weihan Li <weihanli@outlook.com>
2023-06-30 00:27:53 +0800

    cleanup OpenAISample/Helpers

--- AzureSamples/OpenAISample/Helpers.cs
+++ AzureSamples/OpenAISample/Helpers.cs
@@ -4,25 +4,16 @@ namespace OpenAISample;
 
 public static class Helpers
 {
-    public static string? GetInput(string? prompt = null)
+    private static string? GetInput(string? prompt = null, bool insertNewLine = true)
     {
         Console.WriteLine();
         Console.WriteLine(prompt ?? "Enter your input here");
         var input = Console.ReadLine();
-        Console.WriteLine();
-        return input;
-    }
-
-    public static void HandleInputLoop(Action<string> handler, string? inputPrompt = null, string exitInput = "q")
-    {
-        ArgumentNullException.ThrowIfNull(handler);
-        var input = GetInput(inputPrompt);
-        while (input.IsNotNullOrEmpty() && input != exitInput)
+        if (insertNewLine)
         {
-            handler(input);
-
-            input = GetInput(inputPrompt);
+            Console.WriteLine();
         }
+        return input;
     }
 
     public static async Task HandleInputLoopAsync(Func<string, Task<bool>> handler, string? inputPrompt = null, string exitInput = "q")
