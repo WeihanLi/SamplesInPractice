604e5833d94469985290962982baba33b12eeebb
Weihan Li <weihanli@outlook.com>
2023-06-30 17:03:22 +0800

    add GetCosineSimilarity method

--- AzureSamples/OpenAISample/Helpers.cs
+++ AzureSamples/OpenAISample/Helpers.cs
@@ -28,4 +28,22 @@ public static class Helpers
             input = GetInput(inputPrompt);
         }
     }
+
+    // https://github.com/accord-net/framework/blob/49f2932a7baf69c7f6113ec82976aecca0c50353/Sources/Accord.Math/Distances/Cosine.cs#L92
+    public static double GetCosineSimilarity(IList<double> x, IList<double> y)
+    {
+        double sum = 0;
+        double p = 0;
+        double q = 0;
+
+        for (var i = 0; i < x.Count; i++)
+        {
+            sum += x[i] * y[i];
+            p += x[i] * x[i];
+            q += y[i] * y[i];
+        }
+
+        var den = Math.Sqrt(p) * Math.Sqrt(q);
+        return (sum == 0 || den == 0) ? 0 : sum / den;
+    }
 }
