97122b233af6e9ff5a8a08957059c4fe6a54b89a
Weihan Li <weihanli@outlook.com>
2023-07-16 19:18:03 +0800

    feat: use azure openai service directly

--- AzureSamples/OpenAISample/Program.cs
+++ AzureSamples/OpenAISample/Program.cs
@@ -1,8 +1,11 @@
 ﻿using dotenv.net;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenAI;
+using OpenAI.Extensions;
 using OpenAI.Interfaces;
 using OpenAISample;
+using WeihanLi.Common;
 
 // load .env
 DotEnv.Fluent().Load();
@@ -14,13 +17,24 @@ var configuration = new ConfigurationBuilder()
     .AddEnvironmentVariables("OpenAISample_")
     .Build();
 services.AddMemoryCache();
-services.RegisterOpenAIServices(configuration);
+// services.RegisterOpenAIServices(configuration);
+
+// OpenAI proxy
 // services.AddOpenAIService(options =>
 // {
 //     options.ProviderType = ProviderType.OpenAi;
 //     options.ApiKey = Guard.NotNull(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY"));
 //     options.BaseDomain = "https://aoai-proxy.weihanli.xyz";
 // });
+
+// Azure OpenAI, https://github.com/betalgo/openai/wiki/Azure-OpenAI
+services.AddOpenAIService(options =>
+{
+    options.ProviderType = ProviderType.Azure;
+    options.ApiKey = Guard.NotNull(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY"));
+    options.ResourceName = Guard.NotNull(Environment.GetEnvironmentVariable("AZURE_OPENAI_RES_NAME"));
+    options.DeploymentId = Guard.NotNull(Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOY_ID"));
+});
 await using var applicationServices = services.BuildServiceProvider();
 
 var openAIService = applicationServices.GetRequiredService<IOpenAIService>();
