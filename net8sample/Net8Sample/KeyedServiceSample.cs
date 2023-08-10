67aabc5bf64bb7b0b6a611753bc61338e4084e13
Weihan Li <weihanli@outlook.com>
2023-08-10 09:03:10 +0800

    Update KeyedServiceSample

--- net8sample/Net8Sample/KeyedServiceSample.cs
+++ net8sample/Net8Sample/KeyedServiceSample.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Channels;
+using Microsoft.Extensions.Options;
 using WeihanLi.Common;
+using WeihanLi.Common.Helpers;
+using WeihanLi.Common.Otp;
 using WeihanLi.Common.Services;
 
 namespace Net8Sample;
@@ -13,14 +15,43 @@ public static class KeyedServiceSample
         serviceCollection.AddKeyedSingleton<IUserIdProvider, EnvironmentUserIdProvider>("env");
         serviceCollection.AddKeyedSingleton<IUserIdProvider, EnvironmentUserIdProvider>(null);
 
+        serviceCollection.Configure<TotpOptions>(string.Empty, x =>
+        {
+            x.Salt = "1234";
+        });
+        serviceCollection.AddKeyedTransient<ITotpService, TotpService>(string.Empty, (sp, key)=>
+            new TotpService(sp.GetRequiredService<IOptionsMonitor<TotpOptions>>().Get(key?.ToString() ?? Options.DefaultName)));
+            
+        // serviceCollection.AddKeyedService<ITotpService, TotpService, TotpOptions>(string.Empty, options =>
+        // {
+        //     options.Salt = "1234";
+        // });
+        
+        serviceCollection.AddKeyedScoped<IIdGenerator, GuidIdGenerator>("guid");
+        
         using var services = serviceCollection.BuildServiceProvider();
         var userIdProvider = services.GetRequiredKeyedService<IUserIdProvider>(null);
         Console.WriteLine(userIdProvider.GetUserId());
-
+        
         var envUserIdProvider = services.GetRequiredKeyedService<IUserIdProvider>("env");
         Console.WriteLine(envUserIdProvider.GetUserId());
+        Console.WriteLine("userIdProvider == envUserIdProvider ?? {0}", userIdProvider == envUserIdProvider);
 
-        Console.WriteLine("userIdProvider == envUserIdProvider? {0}", envUserIdProvider == userIdProvider);
+        var totpService = services.GetRequiredKeyedService<ITotpService>(string.Empty);
+        Console.WriteLine(totpService.GetCode(Base36Encoder.Encode(Guid.NewGuid())));
+        
+        using var scope = services.CreateScope();
+        var newId = scope.ServiceProvider.GetRequiredKeyedService<IIdGenerator>("guid").NewId();
+        Console.WriteLine(newId);
+    }
+    
+    private static void AddKeyedService<TService, TImplement, TOptions>(this IServiceCollection serviceCollection, string key, Action<TOptions> optionsConfigure)
+        where TService : class
+        where TImplement: class, TService
+        where TOptions: class
+    {
+        serviceCollection.AddKeyedTransient<TService, TImplement>(key);
+        serviceCollection.Configure(key, optionsConfigure);
     }
 }
 
--- net8sample/Net8Sample/Program.cs
+++ net8sample/Net8Sample/Program.cs
@@ -1,10 +1,14 @@
 ﻿using Net8Sample;
+using WeihanLi.Common.Helpers;
+
+InvokeHelper.OnInvokeException =
+    e => ConsoleHelper.InvokeWithConsoleColor(() => Console.WriteLine(e), ConsoleColor.Red);
 
 // JsonSample.MainTest();
 // DataAnnotationSample.MainTest();
 // TimeProviderSample.MainTest();
 
 // MetricsSample.MainTest();
-KeyedServiceSample.MainTest();
+InvokeHelper.TryInvoke(KeyedServiceSample.MainTest);
 
 Console.WriteLine("Hello, World!");
