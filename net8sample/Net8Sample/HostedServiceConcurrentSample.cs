7e41ff8a0ee4c4cdff8741e8f5d9cafcb998813f
Weihan Li <weihanli@outlook.com>
2023-08-13 17:34:16 +0800

    feat: add Net8Sample/HostedServiceConcurrentSample.cs

---
+++ net8sample/Net8Sample/HostedServiceConcurrentSample.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Net8Sample;
+
+public static class HostedServiceConcurrentSample
+{
+    public static async Task MainTest(CancellationToken cancellationToken)
+    {
+        var hostBuilder = Host.CreateEmptyApplicationBuilder(null);
+        hostBuilder.ConfigureHostOptions(x =>
+        {
+            x.ServicesStartConcurrently = true;
+            x.ServicesStopConcurrently = true;
+            x.StartupTimeout = TimeSpan.FromMilliseconds(100);
+        });
+        hostBuilder.Services.AddHostedService<DelayService>();
+        hostBuilder.Services.AddHostedService<Delay2Service>();
+        hostBuilder.Services.AddHostedService<ReportTimeService>();
+        var host = hostBuilder.Build();
+        await host.RunAsync(cancellationToken);
+    }
+    
+    public static IHostApplicationBuilder ConfigureHostOptions(this IHostApplicationBuilder hostBuilder, Action<HostOptions> configureOptions)
+    {
+        hostBuilder.Services.Configure(configureOptions);
+        return hostBuilder;
+    }
+
+    public static IHostApplicationBuilder ConfigureHostOptions(this IHostApplicationBuilder hostBuilder, Action<IHostApplicationBuilder, HostOptions> configureOptions)
+    {
+        hostBuilder.Services.Configure<HostOptions>(options => configureOptions(hostBuilder, options));
+        return hostBuilder;
+    }
+}
+
+file sealed class ReportTimeService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+        while (await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            Console.WriteLine(DateTimeOffset.Now);
+        }
+    }
+}
+
+file sealed class DelayService : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(60), cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+
+file sealed class Delay2Service : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(60), cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
--- net8sample/Net8Sample/Program.cs
+++ net8sample/Net8Sample/Program.cs
@@ -1,9 +1,10 @@
-﻿using Net8Sample;
+﻿using Microsoft.Extensions.Hosting;
+using Net8Sample;
 using WeihanLi.Common.Helpers;
 
 InvokeHelper.OnInvokeException = e => ConsoleHelper.WriteLineWithColor(e.ToString(), ConsoleColor.Red);
 var exitToken = ConsoleHelper.GetExitToken();
-InvokeHelper.TryInvoke(JsonSample.MainTest);
+// InvokeHelper.TryInvoke(JsonSample.MainTest);
 // DataAnnotationSample.MainTest();
 // TimeProviderSample.MainTest();
 
@@ -11,5 +12,6 @@ InvokeHelper.TryInvoke(JsonSample.MainTest);
 // InvokeHelper.TryInvoke(KeyedServiceSample.OptionsSample);
 
 // await InvokeHelper.TryInvokeAsync(() => HostedLifecycleServiceSample.MainTest(exitToken));
+await InvokeHelper.TryInvokeAsync(() => HostedServiceConcurrentSample.MainTest(exitToken));
 
 Console.WriteLine("Hello, World!");
