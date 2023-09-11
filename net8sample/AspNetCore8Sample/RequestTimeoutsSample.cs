92e0167be50e4e1c0ba6e94f806878c2df57db09
Weihan Li <weihanli@outlook.com>
2023-09-11 23:55:33 +0800

    update AspNetCore8Sample

--- net8sample/AspNetCore8Sample/RequestTimeoutsSample.cs
+++ net8sample/AspNetCore8Sample/RequestTimeoutsSample.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.Timeouts;
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http.Timeouts;
 
 namespace AspNetCore8Sample;
 
@@ -12,6 +13,8 @@ public static class RequestTimeoutsSample
             options.DefaultPolicy = new RequestTimeoutPolicy()
             {
                 Timeout = TimeSpan.FromSeconds(1),
+                // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/408
+                // TimeoutStatusCode = 408,
                 WriteTimeoutResponse = context =>
                 {
                     if (context.Response.HasStarted) return Task.CompletedTask;
@@ -20,6 +23,7 @@ public static class RequestTimeoutsSample
             };
             options.AddPolicy("timeout-2s", TimeSpan.FromSeconds(2));
         });
+        builder.Services.AddControllers();
         var app = builder.Build();
         app.UseRequestTimeouts();
         app.Map("/", () => "Hello world");
@@ -34,6 +38,18 @@ public static class RequestTimeoutsSample
             await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
             return Results.Content("No timeout!", "text/plain");
         }).WithRequestTimeout("timeout-2s");
+        app.MapGet("/timeout-attribute-policy", [RequestTimeout("timeout-2s")]async (CancellationToken cancellationToken) =>
+        {
+            try
+            {
+                await Task.Delay(800, cancellationToken);
+            } 
+            catch (TaskCanceledException)
+            {
+                return Results.Content("Timeout!", "text/plain");
+            }
+            return Results.Content("No timeout!", "text/plain");
+        });
         app.MapGet("/timeout-attribute", [RequestTimeout(500)]async (CancellationToken cancellationToken) =>
         {
             try
@@ -47,18 +63,6 @@ public static class RequestTimeoutsSample
             return Results.Content("No timeout!", "text/plain");
         });
         
-        app.MapGet("/no-timeout-attribute", [DisableRequestTimeout]async (CancellationToken cancellationToken) =>
-        {
-            try
-            {
-                await Task.Delay(5000, cancellationToken);
-            } 
-            catch (TaskCanceledException)
-            {
-                return Results.Content("Timeout!", "text/plain");
-            }
-            return Results.Content("No timeout!", "text/plain");
-        });
         app.MapGet("/no-timeout", async (CancellationToken cancellationToken) =>
         {
             try
@@ -71,6 +75,25 @@ public static class RequestTimeoutsSample
             }
             return Results.Content("No timeout!", "text/plain");
         }).DisableRequestTimeout();
+        app.MapGet("/no-timeout-attribute", [DisableRequestTimeout]async (CancellationToken cancellationToken) =>
+        {
+            try
+            {
+                await Task.Delay(5000, cancellationToken);
+            } 
+            catch (TaskCanceledException)
+            {
+                return Results.Content("Timeout!", "text/plain");
+            }
+            return Results.Content("No timeout!", "text/plain");
+        });
+        app.MapGet("/no-timeout-feature", async (HttpContext context) =>
+        {
+            var timeoutFeature = context.Features.GetRequiredFeature<IHttpRequestTimeoutFeature>();
+            timeoutFeature.DisableTimeout();
+            await Task.Delay(3000, context.RequestAborted);
+            return "No timeout";
+        });
 
         app.MapGet("/special-timeout", async (CancellationToken cancellationToken) =>
         {
@@ -84,20 +107,28 @@ public static class RequestTimeoutsSample
         }).WithRequestTimeout(new RequestTimeoutPolicy()
         {
             Timeout = TimeSpan.FromSeconds(1),
-            WriteTimeoutResponse = (context) => context.Response.WriteAsync("Oh it's timeout")
+            TimeoutStatusCode = 418,
+            WriteTimeoutResponse = context => context.Response.WriteAsync("Oh it's timeout")
         });
-        app.MapGet("/special-timeout-policy", async (CancellationToken cancellationToken) =>
+
+        app.MapGet("/no-timeout-infinite", async (CancellationToken cancellationToken) =>
         {
             await Task.Delay(1500, cancellationToken);
-            return "Timeout";
-        }).WithRequestTimeout("timeout-2s");
+            return "No Timeout";
+        }).WithRequestTimeout(Timeout.InfiniteTimeSpan);
 
-        app.MapGet("/timeout-sync",  () =>
+        app.MapGet("/no-timeout-sync",  () =>
         {
             Thread.Sleep(5000);
             return Results.Content("No timeout!", "text/plain");
         });
+        app.MapGet("/no-timeout-async",  async () =>
+        {
+            await Task.Delay(5000);
+            return Results.Content("No timeout!", "text/plain");
+        });
 
+        app.MapControllers();
         await app.RunAsync();
     }
 }
---
+++ net8sample/AspNetCore8Sample/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore8Sample;
+
+[Route("api/values")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet, RequestTimeout(1000)]
+    public async Task<string[]> Values()
+    {
+        await Task.Delay(2000, HttpContext.RequestAborted);
+        return new[] { "test" };
+    }
+    
+    [HttpGet("value"), DisableRequestTimeout]
+    public async Task<string[]> Value()
+    {
+        await Task.Delay(20000, HttpContext.RequestAborted);
+        return new[] { "test" };
+    }
+}
