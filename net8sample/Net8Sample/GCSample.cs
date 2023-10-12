29e4098d90a48ddd2f473b1bfe8de5f27fed63ae
Weihan Li <weihanli@outlook.com>
2023-10-12 09:00:55 +0800

    update Net8Sample/GCSample.cs

--- net8sample/Net8Sample/GCSample.cs
+++ net8sample/Net8Sample/GCSample.cs
@@ -1,24 +1,53 @@
-﻿namespace Net8Sample;
-// https://github.com/dotnet/runtime/issues/70601
+﻿// https://github.com/dotnet/runtime/issues/70601
+// https://learn.microsoft.com/en-us/dotnet/api/System.GC.RefreshMemoryLimit?view=net-8.0
+
+using System.Text.Json;
+
+namespace Net8Sample;
+
 public static class GCSample
 {
     public static void MainTest()
     {
-        var memoryInfo = GC.GetGCMemoryInfo();
-        Console.WriteLine(memoryInfo.TotalAvailableMemoryBytes);
+        PrintMemoryInfo();
         
-        var memoryInBytes = (ulong)50 * 1024 * 1024;
-
-        // If the user has already configured the hard heap limit to something lower then is available 
-        // then make no adjustments to honor the user's setting.
-        if ((ulong)memoryInfo.TotalAvailableMemoryBytes < memoryInBytes)
-            return;
-
-        AppContext.SetData("GCHeapHardLimit", memoryInBytes);
+        ulong memoryInBytes = 50 * 1024 * 1024;
+        SetLimitAndRefresh(memoryInBytes);
         
+        memoryInBytes = 60 * 1024 * 1024;
+        SetLimitAndRefresh(memoryInBytes);
+
+        // System.InvalidOperationException: RefreshMemoryLimit failed with too low hard limit.
+        try
+        {
+            memoryInBytes = 1 * 1024;
+            SetLimitAndRefresh(memoryInBytes);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private static void SetLimitAndRefresh(ulong limit)
+    {
+        // - GCHeapHardLimit
+        // - GCHeapHardLimitPercent
+        // - GCHeapHardLimitSOH
+        // - GCHeapHardLimitLOH
+        // - GCHeapHardLimitPOH
+        // - GCHeapHardLimitSOHPercent
+        // - GCHeapHardLimitLOHPercent
+        // - GCHeapHardLimitPOHPercent
+        AppContext.SetData("GCHeapHardLimit", limit);
         GC.RefreshMemoryLimit();
 
-        memoryInfo = GC.GetGCMemoryInfo();
-        Console.WriteLine(memoryInfo.TotalAvailableMemoryBytes);
+        PrintMemoryInfo();
+    }
+
+    private static void PrintMemoryInfo()
+    {
+        var memoryInfo = GC.GetGCMemoryInfo();
+        Console.WriteLine($"{nameof(memoryInfo.TotalAvailableMemoryBytes)}: {memoryInfo.TotalAvailableMemoryBytes/1024/1024}*1024*1024");
     }
 }
