0c2ec328038d5b7fd5da7f9ef6b934f6f0113927
Weihan Li <weihanli@outlook.com>
2023-11-02 00:01:30 +0800

    update net8sample/AspNetCore8Sample/HttpLoggingInterceptorSample.cs

--- net8sample/AspNetCore8Sample/HttpLoggingInterceptorSample.cs
+++ net8sample/AspNetCore8Sample/HttpLoggingInterceptorSample.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.HttpLogging;
+using System.Diagnostics;
 
 namespace AspNetCore8Sample;
 
@@ -11,13 +12,16 @@ public class HttpLoggingInterceptorSample
         builder.Services.AddHttpLogging(options =>
         {
             options.LoggingFields = HttpLoggingFields.All;
+            // combine the request, request body, response, response body, and duration logs into a single log entry
+            options.CombineLogs = true;
         });
         builder.Services.AddHttpLoggingInterceptor<MyHttpLoggingInterceptor>();
         
         var app = builder.Build();
         app.UseHttpLogging();
+        app.MapGet("/no-log", () => "Hey .NET 8");
         app.MapGet("/hello", () => "Hello");
-        app.MapGet("/crash", () => Results.BadRequest());
+        app.MapGet("/crash", () => Results.BadRequest(new{ TraceId = Activity.Current?.TraceId.ToString() }));
         app.MapGet("/req-intercept", () => "Hello .NET 8");
         app.MapControllers();
         await app.RunAsync();
@@ -29,9 +33,15 @@ file sealed class MyHttpLoggingInterceptor: IHttpLoggingInterceptor
 {
     public ValueTask OnRequestAsync(HttpLoggingInterceptorContext logContext)
     {
-        if (logContext.HttpContext.Request.Path.StartsWithSegments("/req-"))
+        if ("/no-log".Equals(logContext.HttpContext.Request.Path.Value, StringComparison.OrdinalIgnoreCase))
         {
-            logContext.LoggingFields = HttpLoggingFields.Request | HttpLoggingFields.Duration;
+            logContext.LoggingFields = HttpLoggingFields.None;
+        }
+        
+        if (logContext.HttpContext.Request.Path.Value?.StartsWith("/req-") == true)
+        {
+            logContext.LoggingFields = HttpLoggingFields.ResponsePropertiesAndHeaders;
+            logContext.AddParameter("req-path", logContext.HttpContext.Request.Path.Value);
         }
         
         return ValueTask.CompletedTask;
