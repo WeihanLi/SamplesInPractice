f4763a8fb1e89757c451a167f3a58fa74538f7cb
Weihan Li <weihanli@outlook.com>
2023-11-05 00:14:47 +0800

    add Net8Sample/ParallelSample.cs

---
+++ net8sample/Net8Sample/ParallelSample.cs
@@ -0,0 +1,62 @@
+﻿// https://github.com/dotnet/runtime/issues/59019
+// https://github.com/dotnet/runtime/pull/84804
+
+using System.Diagnostics;
+
+namespace Net8Sample;
+
+public class ParallelSample
+{
+    public static async Task MainTest()
+    {
+        // default parallel options
+        Console.WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
+        Console.WriteLine(TimeProvider.System.GetLocalNow().ToString());
+        await Parallel.ForAsync(0, 10, async (_, cancellationToken) =>
+        {
+            await Task.Delay(1000, cancellationToken); 
+        });
+        
+        Console.WriteLine(TimeProvider.System.GetLocalNow().ToString());
+        // MaxDegreeOfParallelism
+        await Parallel.ForAsync(0, 10, new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 10
+            },
+            async (_, cancellationToken) =>
+            {
+                await Task.Delay(1000, cancellationToken);
+            });
+        Console.WriteLine(TimeProvider.System.GetLocalNow().ToString());
+        
+        // cancellation
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(TimeSpan.FromMilliseconds(300));
+        try
+        {
+            await Parallel.ForAsync(0, 10, new ParallelOptions()
+                {
+                    MaxDegreeOfParallelism = 5,
+                    CancellationToken = cts.Token
+                },
+                async (_, cancellationToken) =>
+                {
+                    await Task.Delay(1000, cancellationToken);
+                });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        Console.WriteLine(TimeProvider.System.GetLocalNow().ToString());
+        
+        await Parallel.ForAsync(0, 10, CancellationToken.None, async (i, cancellationToken) =>
+        {
+            Console.WriteLine(i);
+            await Task.CompletedTask.ConfigureAwait(ConfigureAwaitOptions.ForceYielding);
+        });
+        
+        await Parallel.ForEachAsync(Enumerable.Range(0, 10), CancellationToken.None, 
+            async (_, cancellationToken) => { await Task.Delay(1000, cancellationToken); });
+    }
+}
--- net8sample/Net8Sample/Program.cs
+++ net8sample/Net8Sample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using WeihanLi.Common.Helpers;
 
 InvokeHelper.OnInvokeException = e => ConsoleHelper.WriteLineWithColor(e.ToString(), ConsoleColor.Red);
+
 var exitToken = ConsoleHelper.GetExitToken();
 // InvokeHelper.TryInvoke(JsonSample.MainTest);
 // DataAnnotationSample.MainTest();
@@ -12,10 +13,12 @@ var exitToken = ConsoleHelper.GetExitToken();
 // await InvokeHelper.TryInvokeAsync(() => HostedLifecycleServiceSample.MainTest(exitToken));
 // await InvokeHelper.TryInvokeAsync(() => HostedServiceConcurrentSample.MainTest(exitToken));
 
-await InvokeHelper.TryInvokeAsync(ConfigureAwaitOptionsSample.MainTest);
+// await InvokeHelper.TryInvokeAsync(ConfigureAwaitOptionsSample.MainTest);
 
 // await InvokeHelper.TryInvokeAsync(() => WebAppSlimBuilderSample.MainTest(exitToken));
 
 // InvokeHelper.TryInvoke(GCSample.MainTest);
+
+await InvokeHelper.TryInvokeAsync(ParallelSample.MainTest);
 
 Console.WriteLine("Hello, World!");
