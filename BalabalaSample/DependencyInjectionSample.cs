e7973f73926cf6117e54f0388429fdb3ecd292df
Weihan Li <weihanli@outlook.com>
2023-11-05 13:59:24 +0800

    feat: add DependencyInjectionSample

---
+++ BalabalaSample/DependencyInjectionSample.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeihanLi.Common.Helpers;
+using WeihanLi.Common.Services;
+
+namespace BalabalaSample;
+
+public static class DependencyInjectionSample
+{
+    public static async Task MainTest()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<IIdGenerator>(sp => null!);
+        await using var serviceProvider = services.BuildServiceProvider();
+        
+        foreach (var generator in serviceProvider.GetServices<IIdGenerator>())
+        {
+            InvokeHelper.TryInvoke(() => Console.WriteLine(generator.NewId()));
+        }
+    }
+}
