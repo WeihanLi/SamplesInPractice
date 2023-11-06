b1556a14ed9c4c2c1e7fd98fc4f4bb3c426c367e
Weihan Li <weihanli@outlook.com>
2023-11-06 22:34:31 +0800

    update MemoryCacheSample.csproj

--- MemoryCacheSample/Program.cs
+++ MemoryCacheSample/Program.cs
@@ -1,6 +1,27 @@
 ﻿await using var services = new ServiceCollection()
     .AddMemoryCache()
     .BuildServiceProvider();
+var memoryCache = services.GetRequiredService<IMemoryCache>();
+
+Task.Run(() =>
+{
+    memoryCache.GetOrCreate("test", entry =>
+    {
+        Thread.Sleep(4000);
+        Console.WriteLine("completed");
+        return "1213";
+    });
+});
+Thread.Sleep(2000);
+if (memoryCache.TryGetValue("test", out var value))
+{
+    Console.WriteLine($"cache get and value is {value}");
+}
+else
+{
+    Console.WriteLine("not exists");
+}
+
 
 Console.WriteLine("----- Bad -----");
 GetValidValues_Bad(5).Dump();
@@ -12,7 +33,6 @@ GetValidValues_Good(8).Dump();
 
 List<int> GetValidValues_Bad(int threhold)
 {
-    var memoryCache = services.GetRequiredService<IMemoryCache>();
     var values = memoryCache.GetOrCreate("test1", entry =>
     {
         return Enumerable.Range(1, 10).ToList();
@@ -23,7 +43,6 @@ List<int> GetValidValues_Bad(int threhold)
 
 List<int> GetValidValues_Good(int threhold)
 {
-    var memoryCache = services.GetRequiredService<IMemoryCache>();
     var values = memoryCache.GetOrCreate("test", entry =>
     {
         return Enumerable.Range(1, 10).ToList();
