69e5edc41eb69c56ded4ea6410ef8f794aa2d43a
Weihan Li <weihanli@outlook.com>
2023-11-08 23:13:12 +0800

    add asp.net 8 ShortCircuit sample

--- net8sample/AspNetCore8Sample/BasicSetupSample.cs
+++ net8sample/AspNetCore8Sample/BasicSetupSample.cs
@@ -7,6 +7,20 @@ public class BasicSetupSample
         var builder = WebApplication.CreateSlimBuilder(args);
         builder.Services.AddControllers();
         var app = builder.Build();
+        app.Use(async (HttpContext context, RequestDelegate next) =>
+        {
+            context.Response.Headers["Value"] = "123";
+            await next(context);
+        });
+        app.MapGet("/", () => "Hello .NET 8!");
+        // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-8.0#short-circuit-middleware-after-routing
+        // https://github.com/dotnet/aspnetcore/issues/46071
+        // https://github.com/dotnet/aspnetcore/pull/46713
+        // ShortCircuit
+        app.MapGet("/short-circuit", () => "Short circuiting!").ShortCircuit();
+        // MapShortCircuit
+        app.MapShortCircuit(404, "robots.txt", "favicon.ico");
+        
         app.MapControllers();
         await app.RunAsync();
     }
--- net8sample/AspNetCore8Sample/Program.cs
+++ net8sample/AspNetCore8Sample/Program.cs
@@ -1,4 +1,4 @@
 // await RequestTimeoutsSample.MainTest(args);
-// await BasicSetupSample.MainTest(args);
+await BasicSetupSample.MainTest(args);
 // await IdentityApiSample.MainTest(args);
-await HttpLoggingInterceptorSample.MainTest(args);
+// await HttpLoggingInterceptorSample.MainTest(args);
