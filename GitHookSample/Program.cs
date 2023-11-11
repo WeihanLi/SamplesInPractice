2a43ec1add2dfb35f2d507381717fa6877a6cc5f
Weihan Li <weihanli@outlook.com>
2023-11-11 22:45:16 +0800

    feat: configure for iis

--- GitHookSample/Program.cs
+++ GitHookSample/Program.cs
@@ -9,6 +9,13 @@ builder.Services.AddSingleton<GitHookSample.EventHandler>();
 builder.Services.AddSingleton<IEventPublisher>(sp => sp.GetRequiredService<GitHookSample.EventHandler>());
 builder.Services.AddHostedService(sp => sp.GetRequiredService<GitHookSample.EventHandler>());
 
+// configure for IIS
+// https://learn.microsoft.com/aspnet/core/host-and-deploy/iis/in-process-hosting?view=aspnetcore-6.0&WT.mc_id=DT-MVP-5004222#application-configuration
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AutomaticAuthentication = false;
+});
+
 var app = builder.Build();
 app.MapGitHubWebhooks("/api/github/webhooks",app.Configuration.GetRequiredAppSetting("GithubWebhookSecret"));
 app.Map("/", () => "Hooks world");
