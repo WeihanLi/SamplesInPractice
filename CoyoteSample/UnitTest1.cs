05d4daed2cd417d4a0e9deb7090cbd92bd276a23
Weihan Li <weihanli@outlook.com>
2023-12-01 00:04:45 +0800

    update test case

--- CoyoteSample/UnitTest1.cs
+++ CoyoteSample/UnitTest1.cs
@@ -1,15 +1,14 @@
-using Microsoft.Coyote;
 using Microsoft.Coyote.SystematicTesting;
 
 namespace CoyoteSample;
 
 public class UnitTest1
 {
-    private readonly Helper _helper = new();
-
-    [Fact]    
-    public async Task ReadyTest()
+    [Test]
+    [Fact]
+    public async Task NoLockReadyTest()
     {
+        var _helper = new Helper();
         Assert.False(_helper.Ready);
         await Task.Run(_helper.MarkAsReady);
         Assert.True(_helper.Ready);
@@ -17,11 +16,12 @@ public class UnitTest1
 
     [Test]
     [Fact]
-    public void CoyoteTestTask()
+    public async Task LockReadyTest()
     {
-        var configuration = Configuration.Create().WithTestingIterations(1_000_000);
-        var engine = TestingEngine.Create(configuration, ReadyTest);
-        engine.Run();
+        var _helper = new HelperV2();
+        Assert.False(_helper.Ready);
+        await Task.Run(_helper.MarkAsReady);
+        Assert.True(_helper.Ready);
     }
 }
 
@@ -31,5 +31,38 @@ public class Helper
 
     public bool Ready => _ready;
 
-    public void MarkAsReady() => _ready = true;
+    public void MarkAsReady()
+    {
+        _ready = true;
+    }
+}
+
+
+public class HelperV2
+{
+    private readonly object _readyLock = new();
+    private volatile bool _ready;
+
+    public bool Ready
+    {
+        get 
+        {
+            if (_ready) return true;
+
+            lock(_readyLock)
+            {
+                if (_ready) return true;
+                
+                return _ready;
+            }
+        }
+    }
+
+    public void MarkAsReady()
+    {
+        lock (_readyLock)
+        {
+            _ready = true;
+        }
+    }
 }
