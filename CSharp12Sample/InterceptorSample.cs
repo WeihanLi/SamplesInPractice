8573f7774cb2a3f06d4be89c6dc03a68e4d93cef
Weihan Li <weihanli@outlook.com>
2023-12-06 23:54:24 +0800

    update InterceptorSample

--- CSharp12Sample/InterceptorSample.cs
+++ CSharp12Sample/InterceptorSample.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharp12Sample
+﻿using WeihanLi.Common.Models;
+
+namespace CSharp12Sample
 {
     public static class InterceptorSample
     {
@@ -9,6 +11,12 @@
 
             var a = new A();
             a.TestMethod();
+            
+            Console.WriteLine("Hello world");
+            
+            c.GenericSampleMethod(1, Result.Success());
+            
+            C.StaticGenericSampleMethod(1, Result.Success());
         }
     }
     
@@ -26,6 +34,20 @@
         {
             Console.WriteLine("interceptable");
         }
+
+        public void GenericSampleMethod<T1, T2>(T1 t1, T2? t2) 
+            where T1: struct
+            where T2: class
+        {
+            Console.WriteLine($"{nameof(GenericSampleMethod)}, t1: {t1}, t2: {t2}");
+        }
+        
+        public static void StaticGenericSampleMethod<T1, T2>(T1 t1, T2? t2) 
+            where T1: struct
+            where T2: class
+        {
+            Console.WriteLine($"{nameof(GenericSampleMethod)}, t1: {t1}, t2: {t2}");
+        }
     }
 }
 
@@ -39,6 +61,21 @@ namespace CSharp12Sample.Generated
 {
      public static class Extensions
      {
+         // static method intercept sample
+         // [System.Runtime.CompilerServices.InterceptsLocation(@"C:\projects\sources\SamplesInPractice\CSharp12Sample\InterceptorSample.cs", line: 13, character: 21)]
+         // public static void ConsoleWriteLineInterceptor(string msg)
+         // {
+         //     Console.WriteLine($"Intercepted: {msg}");
+         // }
+         //
+         // Error CS9153 : The indicated call is intercepted multiple times.
+         // can not intercept the same invocation more than once
+         // [System.Runtime.CompilerServices.InterceptsLocation(@"C:\projects\sources\SamplesInPractice\CSharp12Sample\InterceptorSample.cs", line: 13, character: 21)]
+         // public static void ConsoleWriteLineInterceptor2(string msg)
+         // {
+         //     Console.WriteLine($"Intercepted2: {msg}");
+         // }
+         
         //  [System.Runtime.CompilerServices.InterceptsLocation(@"C:\projects\sources\SamplesInPractice\CSharp12Sample\InterceptorSample.cs", line: 11/*L1*/, character: 15/*C1*/)] // refers to the call at (L1, C1)
         //  public static void TestMethodInterceptor(this A a)
         //  {
@@ -57,6 +94,22 @@ namespace CSharp12Sample.Generated
 //             Console.WriteLine("Before...");
 //             c.InterceptableMethod();
 //             Console.WriteLine("After...");
-//         }        
+//         }
+
+         // known bug: https://github.com/dotnet/roslyn/issues/70311
+         // [System.Runtime.CompilerServices.InterceptsLocation(@"C:\projects\sources\SamplesInPractice\CSharp12Sample\InterceptorSample.cs", line: 17, character: 15)]
+         // public static void GenericSampleMethodInterceptor<T1, T2>(this C c, T1 t1, T2? t2)
+         //     where T1: struct
+         //     where T2: class
+         // {
+         //     Console.WriteLine($"Intercepted: {nameof(GenericSampleMethodInterceptor)}");
+         // }
+         
+         //
+         // [System.Runtime.CompilerServices.InterceptsLocation(@"C:\projects\sources\SamplesInPractice\CSharp12Sample\InterceptorSample.cs", line: 19, character: 15)]
+         // public static void StaticGenericSampleMethodInterceptor<T1, T2>(T1 t1, T2 t2)
+         // {
+         //     Console.WriteLine($"Intercepted: {nameof(StaticGenericSampleMethodInterceptor)}");
+         // }
      }
 }
--- CSharp12Sample/Program.cs
+++ CSharp12Sample/Program.cs
@@ -2,7 +2,7 @@
 // UsingAliasAnyTypeSample.MainTest();
 // CollectionLiteralSample.MainTest();
 // ExperimentalSample.MainTest();
-// InterceptorSample.MainTest();
-await ServiceScopeActivitySample.MainTest();
+InterceptorSample.MainTest();
+// await ServiceScopeActivitySample.MainTest();
 
 Console.WriteLine("Hello, World!");
