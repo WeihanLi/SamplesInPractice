1197e5c2c6f79ff61c94a16ce8cd23aaafb49eeb
Weihan Li <weihanli@outlook.com>
2023-12-29 18:32:59 +0800

    add BalabalaSample/ConfigureOptionsSample.cs

---
+++ BalabalaSample/ConfigureOptionsSample.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace BalabalaSample;
+
+public class ConfigureOptionsSample
+{
+    public static void MainTest()
+    {
+        var services = new ServiceCollection();
+        
+        services.Configure<NumberOption>(null,o =>
+        {
+            o.Num = 2;
+        });
+        
+        
+        services.Configure<NumberOption>(o =>
+        {
+            o.Num++;
+        });
+        
+        services.Configure<NumberOption>("h", o =>
+        {
+            o.Num++;
+        });
+        using var sp = services.BuildServiceProvider();
+        
+        Console.WriteLine(sp.GetRequiredService<IOptionsMonitor<NumberOption>>()
+            .Get("h").Num);
+    }
+}
+
+
+file sealed class NumberOption
+{
+    public int Num { get; set; }
+}
