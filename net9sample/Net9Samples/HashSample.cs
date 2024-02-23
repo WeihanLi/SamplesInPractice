a7e0d8928811b68b7fb3d49777cf042a373e3cc7
Weihan Li <weihanli@outlook.com>
2024-02-23 15:32:49 +0800

    add Net9Samples/HashSample.cs

---
+++ net9sample/Net9Samples/HashSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace Net9Samples;
+
+// https://github.com/dotnet/runtime/issues/91407
+// https://github.com/dotnet/runtime/pull/92430
+public static class HashSample
+{
+    public static void MainTest()
+    {
+        var helloBytes = "Hello World"u8;
+        var sha1Bytes = SHA1.HashData(helloBytes);
+
+        var hashedBytes = CryptographicOperations.HashData(HashAlgorithmName.SHA1, helloBytes);
+        Console.WriteLine(hashedBytes.SequenceEqual(sha1Bytes));
+        
+        var keysBytes = "keys"u8;
+        var hmacBytes = CryptographicOperations.HmacData(HashAlgorithmName.SHA1, keysBytes, helloBytes);
+        Console.WriteLine(Convert.ToHexString(hmacBytes));
+    }
+}
