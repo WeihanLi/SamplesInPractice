dbab5dab5dfd7c229714f163001594de78450c0a
Weihan Li <weihanli@outlook.com>
2024-02-23 15:33:10 +0800

    add Net9Samples/TaskSample.cs

---
+++ net9sample/Net9Samples/TaskSample.cs
@@ -0,0 +1,27 @@
+﻿namespace Net9Samples;
+
+// https://github.com/dotnet/runtime/issues/47998
+// https://github.com/dotnet/runtime/pull/97077
+public class TaskSample
+{
+    public static async Task MainTest()
+    {
+        {
+            var tcs = new TaskCompletionSource();
+
+            _ = Task.Delay(2000).ContinueWith(r=> tcs.SetFromTask(r));
+
+            await tcs.Task;
+            Console.WriteLine("Completed");
+        }
+        
+        {
+            var tcs = new TaskCompletionSource();
+            using var cts = new CancellationTokenSource(200);
+            _ = Task.Delay(2000, cts.Token).ContinueWith(r=> tcs.SetFromTask(r));
+
+            await tcs.Task;
+            Console.WriteLine("Completed");
+        }
+    }
+}
