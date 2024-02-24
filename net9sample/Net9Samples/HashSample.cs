c45f5e1482ece908b6bcbb7df34c54163e338653
Weihan Li <weihanli@outlook.com>
2024-02-24 13:50:17 +0800

    update Net9Samples/HashSample.cs

--- net9sample/Net9Samples/HashSample.cs
+++ net9sample/Net9Samples/HashSample.cs
@@ -9,13 +9,52 @@ public static class HashSample
     public static void MainTest()
     {
         var helloBytes = "Hello World"u8;
-        var sha1Bytes = SHA1.HashData(helloBytes);
-
-        var hashedBytes = CryptographicOperations.HashData(HashAlgorithmName.SHA1, helloBytes);
-        Console.WriteLine(hashedBytes.SequenceEqual(sha1Bytes));
+        var keyBytes = "keys"u8;
         
-        var keysBytes = "keys"u8;
-        var hmacBytes = CryptographicOperations.HmacData(HashAlgorithmName.SHA1, keysBytes, helloBytes);
-        Console.WriteLine(Convert.ToHexString(hmacBytes));
+        var sha1Bytes = SHA1.HashData(helloBytes);
+        var sha1HashedBytes = CryptographicOperations.HashData(HashAlgorithmName.SHA1, helloBytes);
+        Console.WriteLine(sha1HashedBytes.SequenceEqual(sha1Bytes));
+        
+        var sha1MacBytes = HMACSHA1.HashData(keyBytes, helloBytes);
+        var sha1HmacBytes = CryptographicOperations.HmacData(HashAlgorithmName.SHA1, keyBytes, helloBytes);
+        Console.WriteLine(sha1HmacBytes.SequenceEqual(sha1MacBytes));
+        
+        PrintHashString(HashAlgorithmName.MD5, helloBytes);
+        PrintHashString(HashAlgorithmName.SHA1, helloBytes);
+        PrintHashString(HashAlgorithmName.SHA256, helloBytes);
+        PrintHashString(HashAlgorithmName.SHA384, helloBytes);
+        PrintHashString(HashAlgorithmName.SHA512, helloBytes);
+        // sha3, https://cryptobook.nakov.com/cryptographic-hash-functions/secure-hash-algorithms
+        PrintHashString(HashAlgorithmName.SHA3_256, helloBytes);
+        PrintHashString(HashAlgorithmName.SHA3_384, helloBytes);
+        PrintHashString(HashAlgorithmName.SHA3_512, helloBytes);
+        
+        // hmac
+        Console.WriteLine();
+        Console.WriteLine("HMAC samples:");
+        PrintHmacString(HashAlgorithmName.MD5, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA1, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA256, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA384, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA512, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA3_256, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA3_384, keyBytes, helloBytes);
+        PrintHmacString(HashAlgorithmName.SHA3_512, keyBytes, helloBytes);
+
+        Console.WriteLine("Completed");
+    }
+
+    private static void PrintHashString(HashAlgorithmName hashAlgorithmName, ReadOnlySpan<byte> bytes)
+    {
+        var hashedBytes = CryptographicOperations.HashData(hashAlgorithmName, bytes);
+        var hexString = Convert.ToHexString(hashedBytes);
+        Console.WriteLine($"{hashAlgorithmName}, {hexString}");
+    }
+    
+    private static void PrintHmacString(HashAlgorithmName hashAlgorithmName, ReadOnlySpan<byte> key, ReadOnlySpan<byte> bytes)
+    {
+        var hashedBytes = CryptographicOperations.HmacData(hashAlgorithmName, key, bytes);
+        var hexString = Convert.ToHexString(hashedBytes);
+        Console.WriteLine($"{hashAlgorithmName}, {hexString}");
     }
 }
