e30f3d601018fade359aa3dc3ae4d66598953ddc
Weihan Li <weihanli@outlook.com>
2024-02-25 14:25:18 +0800

    add Net9Samples/AssemblyBuilderSampler.cs

---
+++ net9sample/Net9Samples/AssemblyBuilderSampler.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Net9Samples;
+
+public static class AssemblyBuilderSampler
+{
+    public static void MainTest()
+    {
+        var testAssemblyName = $"test-{Guid.NewGuid():N}";
+        var assemblyBuilder = AssemblyBuilder.DefinePersistedAssembly(new AssemblyName(testAssemblyName), typeof(object).Assembly);
+        var moduleBuilder = assemblyBuilder.DefineDynamicModule("Main");
+        var typeBuilder = moduleBuilder.DefineType("TestType", TypeAttributes.Public | TypeAttributes.Sealed);
+        var type = typeBuilder.CreateType();
+        type.GetMethod("Test")?.Invoke(null, []);
+        assemblyBuilder.Save($"{testAssemblyName}.dll");
+    }
+}
