7450647a365100f8e7726a70665b6be9df071899
Weihan Li <weihanli@outlook.com>
2024-03-18 22:59:44 +0800

    feat: update UpgradeAllTools
    support json output

--- CommandLineAppSamples/UpgradeAllTools/Program.cs
+++ CommandLineAppSamples/UpgradeAllTools/Program.cs
@@ -1,4 +1,5 @@
-﻿using WeihanLi.Common.Helpers;
+﻿using WeihanLi.Common;
+using WeihanLi.Common.Helpers;
 using WeihanLi.Extensions;
 using static WeihanLi.Common.Helpers.ConsoleHelper;
 
@@ -29,29 +30,21 @@ var isLocal = args is ["--local"];
 var toolLevelOption = isLocal ? "--local" : "--global";
 Console.WriteLine($"Going to update all {toolLevelOption.TrimStart('-')} tools");
 
-var exitToken = InvokeHelper.GetExitToken();
-var dotnetPath = ApplicationHelper.GetDotnetPath();
-ArgumentNullException.ThrowIfNull(dotnetPath);
-var dotnetToolListOutput = await CommandExecutor.ExecuteAndCaptureAsync(dotnetPath, $"tool list {toolLevelOption}", cancellationToken: exitToken);
-Console.WriteLine($"`dotnet tool list {toolLevelOption}` output:");
-Console.WriteLine(dotnetToolListOutput.StandardOut);
-
-var dotnetToolList = dotnetToolListOutput.StandardOut.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-if (dotnetToolList.Length <= 2) return;
-
-foreach (var tool in dotnetToolList[2..])
+var dotnetPath = Guard.NotNullOrEmpty(ApplicationHelper.GetDotnetPath());
+var tools = await GetPackagesAsync();
+foreach (var tool in tools)
 {
-    var toolId = tool.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0].Trim();
-    if (thisToolId.Equals(toolId))
+    if (thisToolId.Equals(tool.PackageId))
         continue;
 
+    var toolId = tool.PackageId;
     Console.WriteLine($"update tool {toolId}...");
     try
     {
         await RetryHelper.TryInvokeAsync(async () =>
         {
             var exitCode = await CommandExecutor.ExecuteAsync(dotnetPath, $"tool update {toolLevelOption} {toolId}",
-                cancellationToken: exitToken);
+                cancellationToken: ApplicationHelper.ExitToken);
             exitCode.EnsureSuccessExitCode();
         });
         Console.WriteLine($"update tool {toolId} completed");
@@ -70,3 +63,63 @@ static void PrintHelp()
     Console.WriteLine($"\t {"-v/--version",-20} \t output version information");
     Console.WriteLine($"\t {"-h/--help",-20} \t output help information");
 }
+
+async Task<ToolListJsonContract[]> GetPackagesAsync()
+{
+#if NET9_0_OR_GREATER
+    var jsonResult = await CommandExecutor.ExecuteAndCaptureAsync(dotnetPath, $"tool list {toolLevelOption} --format=json", cancellationToken: ApplicationHelper.ExitToken);
+    if (jsonResult.ExitCode is 0)
+    {
+        var result = System.Text.Json.JsonSerializer.Deserialize<ToolListJsonWithVersionContract>(jsonResult.StandardOut);
+        Guard.NotNull(result);
+        if (result.Version != 1)
+        {
+            throw new NotSupportedException($"Version {result.Version} is not supported now");
+        }
+        return result.Data;
+    }
+#endif
+
+    var tableResult = await CommandExecutor.ExecuteAndCaptureAsync(dotnetPath, $"tool list {toolLevelOption}",
+        cancellationToken: ApplicationHelper.ExitToken);
+    var packages = tableResult.StandardOut.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+        .Skip(2)
+        .Select(line =>
+        {
+            var lineSplits = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return new ToolListJsonContract
+            {
+                PackageId = lineSplits[0], 
+                Version = lineSplits[1],
+                Commands = [lineSplits[2]]
+            };
+        }).ToArray();
+    return packages;
+}
+
+[System.Diagnostics.DebuggerDisplay("{PackageId}, {Version}")]
+internal sealed class ToolListJsonContract
+{
+    [System.Text.Json.Serialization.JsonPropertyName("packageId")]
+    public required string PackageId { get; init; }
+
+    [System.Text.Json.Serialization.JsonPropertyName("version")]
+    public required string Version { get; init; }
+
+    [System.Text.Json.Serialization.JsonPropertyName("commands")]
+    public required string[] Commands { get; init; }
+}
+
+#if NET9_0_OR_GREATER
+internal sealed class ToolListJsonWithVersionContract
+{
+    /// <summary>
+    /// The version of the JSON format for dotnet tool list.
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("version")]
+    public int Version { get; init; } = 1;
+
+    [System.Text.Json.Serialization.JsonPropertyName("data")]
+    public required ToolListJsonContract[] Data { get; init; }
+}
+#endif
