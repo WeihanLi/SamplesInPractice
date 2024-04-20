e2465c2826f3fa075855bd9f537f2b8facaeae85
Weihan Li <weihanli@outlook.com>
2024-04-20 09:33:16 +0800

    try on NetworkInterface.GetAllNetworkInterfaces to get all interface ip

--- IpMonitor/Worker.cs
+++ IpMonitor/Worker.cs
@@ -1,4 +1,8 @@
 using System.Net;
+using System.Net.NetworkInformation;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WeihanLi.Common.Helpers;
 using WeihanLi.Extensions;
 using WeihanLi.Extensions.Hosting;
 
@@ -30,6 +34,25 @@ public sealed class Worker : TimerBaseBackgroundServiceWithDiagnostic
 
     protected override async Task ExecuteTaskInternalAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
+        foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                continue;
+            
+            var physicalAddress = networkInterface.GetPhysicalAddress();
+            Console.WriteLine(physicalAddress);
+            var ipProperties = networkInterface.GetIPProperties();
+            var ipV4Properties = ipProperties.GetIPv4Properties();
+            var ipV6Properties = ipProperties.GetIPv6Properties();
+            Console.WriteLine(JsonSerializer.Serialize(ipProperties));
+        }
+
+        var interfaceInfo = JsonSerializer.Serialize(NetworkInterface.GetAllNetworkInterfaces(), new JsonSerializerOptions(JsonSerializerDefaults.General)
+        {
+            Converters = { new JsonStringEnumConverter() }
+        }.WithUnsafeEncoder());
+        // Console.WriteLine(interfaceInfo);
+        
         var host = Dns.GetHostName();
         var ips = await Dns.GetHostAddressesAsync(host, cancellationToken);
         var ipInfo = $"{Environment.MachineName} - {host}\n {ips.Order(new IpAddressComparer())
