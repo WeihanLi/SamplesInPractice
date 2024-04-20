9fa3e906bc32ee10178092e7eb28a8b66646e563
Weihan Li <weihanli@outlook.com>
2024-04-20 17:08:14 +0800

    fix build

--- net9sample/Net9Samples/AssemblyBuilderSampler.cs
+++ net9sample/Net9Samples/AssemblyBuilderSampler.cs
@@ -8,7 +8,7 @@ public static class AssemblyBuilderSampler
     public static void MainTest()
     {
         var testAssemblyName = $"test-{Guid.NewGuid():N}";
-        var assemblyBuilder = AssemblyBuilder.DefinePersistedAssembly(new AssemblyName(testAssemblyName), typeof(object).Assembly);
+        var assemblyBuilder = new PersistedAssemblyBuilder(new AssemblyName(testAssemblyName), typeof(object).Assembly);
         var moduleBuilder = assemblyBuilder.DefineDynamicModule("Main");
         var typeBuilder = moduleBuilder.DefineType("TestType", TypeAttributes.Public | TypeAttributes.Sealed);
         var type = typeBuilder.CreateType();
