fb6314a7c01489216c2914cd25bbd2849d981537
Weihan Li <weihanli@outlook.com>
2024-05-07 00:14:40 +0800

    add CSharp13Samples.cs

---
+++ net9sample/Net9Samples/CSharp13Samples.cs
@@ -0,0 +1,17 @@
+﻿namespace Net9Samples;
+
+public static class CSharp13Samples
+{
+    public static void ParamsCollectionSample()
+    {
+        ParamsMethod(1, 2, 3);
+
+        void ParamsMethod(params Span<int> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
