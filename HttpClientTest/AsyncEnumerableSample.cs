b5f31c5a92324971d0b51c33c011a2a790f530fb
Weihan Li <weihanli@outlook.com>
2024-05-11 21:03:58 +0800

    update HttpClientTest

--- HttpClientTest/AsyncEnumerableSample.cs
+++ HttpClientTest/AsyncEnumerableSample.cs
@@ -1,4 +1,8 @@
-﻿using System.Net;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Hosting;
+using System.Net;
 using System.Net.Http.Json;
 using WeihanLi.Common.Helpers;
 
@@ -8,10 +12,23 @@ public static class AsyncEnumerableSample
 {
     public static async Task MainTest()
     {
-        var url = "http://localhost:5000/api/Values/en-US/single/IDS_WEIHAN_TEST/allCountries?includeGlobal=true";
+        var server = await StartupServer();
+        
+        var url = "http://localhost:5000/api/values";
+        var url1 = "http://localhost:5000/api/values/async";
         {
             Console.WriteLine($"Send request at {DateTimeOffset.Now}");
             using var httpClient = new HttpClient();
+            await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
+            {
+                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
+            }
+        }
+        Console.WriteLine(new string('-', 50));
+        {
+            // https://github.com/dotnet/runtime/issues/102113
+            Console.WriteLine($"Send request at {DateTimeOffset.Now}");
+            using var httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.All });
             await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
             {
                 Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
@@ -21,53 +38,76 @@ public static class AsyncEnumerableSample
         Console.WriteLine(new string('-', 50));
         {
             Console.WriteLine($"Send request at {DateTimeOffset.Now}");
-            var postUrl = "http://localhost:5000/api/Values/en-US/entry/allCountries";
             using var httpClient = new HttpClient();
-            using var response = await httpClient.PostAsync(postUrl,
-                JsonContent.Create(new { cmsKeyNames = new[] { "IDS_CMS_SDK_TEST", "IDS_WEIHAN_TEST" } }));
-            await foreach (var entry in response.Content.ReadFromJsonAsAsyncEnumerable<KeyEntry>())
+            await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url1))
             {
-                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {entry}");
+                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
             }
         }
-        
         Console.WriteLine(new string('-', 50));
         {
+            // https://github.com/dotnet/runtime/issues/102113
             Console.WriteLine($"Send request at {DateTimeOffset.Now}");
-            var postUrl = "http://localhost:5000/api/Values/en-US/entry/allCountries";
-            using var httpClient = new HttpClient();
-            using var requestContent = JsonContent.Create(new { cmsKeyNames = new[] { "IDS_CMS_SDK_TEST", "IDS_WEIHAN_TEST" } }); 
-            using var request = new HttpRequestMessage(HttpMethod.Post, postUrl);
-            request.Content = requestContent;
-            using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-            await foreach (var entry in response.Content.ReadFromJsonAsAsyncEnumerable<KeyEntry>())
+            using var httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.All });
+            await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url1))
             {
-                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {entry}");
+                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
             }
         }
 
-        // Console.WriteLine(new string('-', 50));
-        //
-        // {
-        //     Console.WriteLine($"Send request at {DateTimeOffset.Now}");
-        //     await foreach (var (key, value) in HttpHelper.HttpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
-        //     {
-        //         Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
-        //     }
-        // }
-        
-        // Console.WriteLine(new string('-', 50));
-        //
-        // {
-        //     // https://github.com/dotnet/runtime/issues/102113
-        //     Console.WriteLine($"Send request at {DateTimeOffset.Now}");
-        //     using var httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.All });
-        //     await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
-        //     {
-        //         Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
-        //     }
-        // }
+        await server.StopAsync();
+    }
+    
+    private static async Task<WebApplication> StartupServer()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.Logging.ClearProviders();
+        builder.Services.AddControllers();
+        builder.Services.AddResponseCompression(options =>
+        {
+            options.Providers.Add<GzipCompressionProvider>();
+        });
+        var app = builder.Build();
+        app.Urls.Clear();
+        app.Urls.Add("http://localhost:5000");
+        app.UseResponseCompression();
+        app.Map("/m/api/values", TestHelper.GetValues);
+        app.Map("/m/api/values1", TestHelper.GetValuesAsync);
+        app.MapControllers();
+        await app.StartAsync();
+        Console.WriteLine("Server started");
+        return app;
     }
 }
 
-file sealed record KeyEntry(string Key, string Value, string Country);
+file static class TestHelper
+{
+    public static async IAsyncEnumerable<KeyValuePair<string, string>> GetValues()
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            yield return new KeyValuePair<string, string>("test", "test");
+            await Task.CompletedTask.ConfigureAwait(ConfigureAwaitOptions.ForceYielding);
+            Thread.Sleep(2000);
+        }
+    }
+        
+    public static async IAsyncEnumerable<KeyValuePair<string, string>> GetValuesAsync()
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            yield return new KeyValuePair<string, string>("test", "async test");
+            await Task.Delay(2000);
+        }
+    }
+}
+
+[Route("api/values")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IAsyncEnumerable<KeyValuePair<string, string>> GetValues() => TestHelper.GetValues();
+    
+    [HttpGet("async")]
+    public IAsyncEnumerable<KeyValuePair<string, string>> GetValuesAsync() => TestHelper.GetValuesAsync();
+}
---
+++ HttpClientTest/AsyncEnumerableSample2.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Net.Http.Json;
+using WeihanLi.Common.Helpers;
+
+namespace HttpClientTest;
+
+public static class AsyncEnumerableSample2
+{
+    public static async Task MainTest()
+    {
+        var url = "http://localhost:5000/api/values/en-US/single/IDS_WEIHAN_TEST/allCountries";
+        {
+            Console.WriteLine($"Send request at {DateTimeOffset.Now}");
+            using var httpClient = new HttpClient();
+            await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
+            {
+                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
+            }
+        }
+        Console.WriteLine(new string('-', 50));
+        {
+            // https://github.com/dotnet/runtime/issues/102113
+            Console.WriteLine($"Send request at {DateTimeOffset.Now}");
+            using var httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.All });
+            await foreach (var (key, value) in httpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
+            {
+                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
+            }
+        }
+        Console.WriteLine(new string('-', 50));
+        {
+            Console.WriteLine($"Send request at {DateTimeOffset.Now}");
+            await foreach (var (key, value) in HttpHelper.HttpClient.GetFromJsonAsAsyncEnumerable<KeyValuePair<string, string>>(url))
+            {
+                Console.WriteLine($"Received response at {DateTimeOffset.Now} : {key} {value}");
+            }
+        }
+        
+        // Console.WriteLine(new string('-', 50));
+        // {
+        //     Console.WriteLine($"Send request at {DateTimeOffset.Now}");
+        //     var postUrl = "http://localhost:5000/api/Values/en-US/entry/allCountries";
+        //     using var httpClient = new HttpClient();
+        //     using var response = await httpClient.PostAsync(postUrl,
+        //         JsonContent.Create(new { cmsKeyNames = new[] { "IDS_CMS_SDK_TEST", "IDS_WEIHAN_TEST" } }));
+        //     await foreach (var entry in response.Content.ReadFromJsonAsAsyncEnumerable<KeyEntry>())
+        //     {
+        //         Console.WriteLine($"Received response at {DateTimeOffset.Now} : {entry}");
+        //     }
+        // }
+        //
+        // Console.WriteLine(new string('-', 50));
+        // {
+        //     Console.WriteLine($"Send request at {DateTimeOffset.Now}");
+        //     var postUrl = "http://localhost:5000/api/Values/en-US/entry/allCountries";
+        //     using var httpClient = new HttpClient();
+        //     using var requestContent = JsonContent.Create(new { cmsKeyNames = new[] { "IDS_CMS_SDK_TEST", "IDS_WEIHAN_TEST" } }); 
+        //     using var request = new HttpRequestMessage(HttpMethod.Post, postUrl);
+        //     request.Content = requestContent;
+        //     using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+        //     await foreach (var entry in response.Content.ReadFromJsonAsAsyncEnumerable<KeyEntry>())
+        //     {
+        //         Console.WriteLine($"Received response at {DateTimeOffset.Now} : {entry}");
+        //     }
+        // }
+
+    }
+}
+
+file sealed record KeyEntry(string Key, string Value, string Country);
