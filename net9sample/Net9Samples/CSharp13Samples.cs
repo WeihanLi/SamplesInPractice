ea5616a30a2f89b802afa5178dd68911b5452234
Weihan Li <weihanli@outlook.com>
2024-06-13 23:18:30 +0800

    update CSharp13Samples

--- net9sample/Net9Samples/CSharp13Samples.cs
+++ net9sample/Net9Samples/CSharp13Samples.cs
@@ -4,14 +4,56 @@ public static class CSharp13Samples
 {
     public static void ParamsCollectionSample()
     {
-        ParamsMethod(1, 2, 3);
+        ParamsArrayMethod(1, 2, 3);
+        ParamsListMethod(1, 2, 3);
+        ParamsEnumerableMethod(1, 2, 3);
 
-        void ParamsMethod(params Span<int> collection)
+        ParamsReadOnlySpanMethod(1, 2, 3);
+        ParamsSpanMethod(1, 2, 3);
+
+        void ParamsReadOnlySpanMethod(params Span<int> collection)
         {
             foreach (var item in collection)
             {
                 Console.WriteLine(item);
             }
         }
+
+        void ParamsSpanMethod(params Span<int> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        void ParamsListMethod(params List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        void ParamsEnumerableMethod(params IEnumerable<int> array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        void ParamsArrayMethod(params int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
+
+
+    //public class SemiFieldSample
+    //{
+    //    public string Name { get; set => field = value.Trim(); }
+    //}
 }
