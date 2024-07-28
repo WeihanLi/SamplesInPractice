016f414ccefd222ec2cac0b1c52acac76545c9c1
Weihan Li <weihanli@outlook.com>
2024-07-28 17:58:24 +0800

    add ForwardedHeadersSample.cs

---
+++ net8sample/AspNetCore8Sample/ForwardedHeadersSample.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+
+namespace AspNetCore8Sample;
+
+public static class ForwardedHeadersSample
+{
+    public static async Task MainTest(string[] args)
+    {
+        // Environment.SetEnvironmentVariable("ASPNETCORE_USEFORWARDEDHEADHERS_ENABLED", "true");
+        var app = WebApplication.Create(args);
+        app.Map("/", (HttpContext context) => new
+        {
+            Url = context.Request.GetDisplayUrl()
+        });
+        await app.RunAsync();
+    }
+}
--- net8sample/AspNetCore8Sample/Program.cs
+++ net8sample/AspNetCore8Sample/Program.cs
@@ -1,10 +1,9 @@
-using WeihanLi.Extensions;
-
-Console.WriteLine(Enum.GetNames<Level>().StringJoin(","));
-Console.WriteLine(Level.系统.ToString());
+﻿//Console.WriteLine(Enum.GetNames<Level>().StringJoin(","));
+//Console.WriteLine(Level.系统.ToString());
 
 // await RequestTimeoutsSample.MainTest(args);
 // await BasicSetupSample.MainTest(args);
 // await IdentityApiSample.MainTest(args);
 // await HttpLoggingInterceptorSample.MainTest(args);
-await ExceptionHandlerSample.MainTest(args);
+// await ExceptionHandlerSample.MainTest(args);
+await ForwardedHeadersSample.MainTest(args);
