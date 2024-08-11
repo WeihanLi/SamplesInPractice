bdd68ecc3255805bea37dcfbec9218c6696cb8c3
Weihan Li <weihanli@outlook.com>
2024-08-11 22:32:25 +0800

    add JsonSchemaExporterTest

--- net9sample/Net9Samples/JsonSample.cs
+++ net9sample/Net9Samples/JsonSample.cs
@@ -1,4 +1,8 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+using WeihanLi.Common.Helpers;
 
 namespace Net9Samples;
 
@@ -11,7 +15,7 @@ public static class JsonSample
             Id = 1,
             Title = "Engineer"
         };
-        
+
         var indentOptions = new JsonSerializerOptions
         {
             WriteIndented = true
@@ -24,10 +28,63 @@ public static class JsonSample
             IndentSize = 1
         };
         Console.WriteLine(JsonSerializer.Serialize(job, indentOptions1));
-        
+
         Console.WriteLine();
-        
+
         Console.WriteLine(JsonSerializer.Serialize(job));
         Console.WriteLine(JsonSerializer.Serialize(job, JsonSerializerOptions.Web));
     }
+
+    public static void JsonSchemaExporterTest()
+    {
+        var type = typeof(Job);
+        var schemaSerializeOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web).WithWriteIntended();
+
+        var schemaNode = JsonSerializerOptions.Web.GetJsonSchemaAsNode(typeof(Job));
+        Console.WriteLine(JsonSerializer.Serialize(schemaNode, schemaSerializeOptions));
+
+        var schemaNode2 = JsonSerializerOptions.Default.GetJsonSchemaAsNode(typeof(Job));
+        Console.WriteLine(JsonSerializer.Serialize(schemaNode2, schemaSerializeOptions));
+
+        var exporterOptions = new JsonSchemaExporterOptions
+        {
+            TransformSchemaNode = (context, jsonNode) =>
+            {
+                var node = jsonNode.DeepClone();
+                var idNames = new[] { "id", "Id" };
+
+                if (node["properties"] is not JsonObject propertiesNode)
+                    return node;
+
+                foreach (var idName in idNames)
+                {
+                    if (propertiesNode[idName] is JsonObject)
+                    {
+                        var requiredNode = node["required"];
+                        if (requiredNode is JsonArray jsonArrayNode)
+                        {
+                            var requiredProperties = JsonSerializer.Serialize(jsonArrayNode.Select(x => x.GetValue<string>()).Append(idName));
+                            jsonArrayNode.ReplaceWith(JsonSerializer.Deserialize<JsonArray>(requiredProperties));
+                        }
+                        else
+                        {
+                            node["required"] = JsonSerializer.Deserialize<JsonArray>($"""["{idName}"]""");
+                        }
+                    }
+
+                }
+                return node;
+            }
+        };
+        var schemaNode3 = JsonSerializerOptions.Default.GetJsonSchemaAsNode(typeof(Job), exporterOptions);
+        Console.WriteLine(JsonSerializer.Serialize(schemaNode3, schemaSerializeOptions));
+    }
+}
+
+
+file class Job
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
 }
