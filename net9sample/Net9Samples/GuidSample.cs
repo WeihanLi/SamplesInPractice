fb14d699bde46d41d27e080c363a45f69107d5b0
Weihan Li <weihanli@outlook.com>
2024-08-19 21:46:05 +0800

    add Net9Samples/GuidSample

---
+++ net9sample/Net9Samples/GuidSample.cs
@@ -0,0 +1,13 @@
+﻿namespace Net9Samples;
+internal static class GuidSample
+{
+    // https://datatracker.ietf.org/doc/rfc9562/
+    // https://www.rfc-editor.org/rfc/rfc9562.html
+    // https://github.com/dotnet/runtime/issues/103658
+    // https://github.com/dotnet/runtime/pull/104124
+    public static void MainTest()
+    {
+        var guid = Guid.CreateVersion7();
+        Console.WriteLine(guid.ToString());
+    }
+}
