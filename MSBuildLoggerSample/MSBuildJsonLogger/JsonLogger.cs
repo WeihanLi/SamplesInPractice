6d8a112a45618c37a5875a9da42d222ad949896d
Weihan Li <weihanli@outlook.com>
2024-09-08 22:31:14 +0800

    add MSBuildLoggerSample/MSBuildJsonLogger

---
+++ MSBuildLoggerSample/MSBuildJsonLogger/JsonLogger.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Build.Framework;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace MSBuildJsonLogger;
+
+public sealed class JsonErrorLogger : ILogger
+{
+    private const string ErrorLogFileName = "json-error-logger.json";
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+    
+    private readonly List<BuildError> _errors = new();
+    public void Initialize(IEventSource eventSource)
+    {
+        eventSource.ErrorRaised += EventSourceOnErrorRaised;
+    }
+
+    private void EventSourceOnErrorRaised(object sender, BuildErrorEventArgs e)
+    {
+        _errors.Add(new BuildError(e.Subcategory, e.Code, e.File, e.LineNumber, e.ColumnNumber, e.EndLineNumber, e.EndColumnNumber, e.Message, e.HelpKeyword, e.SenderName));
+    }
+
+    public void Shutdown()
+    {
+        using var fs = File.Create(ErrorLogFileName);
+        JsonSerializer.Serialize(fs, _errors, JsonSerializerOptions);
+        
+        _errors.Clear();
+    }
+
+    public LoggerVerbosity Verbosity { get; set; }
+    public string? Parameters { get; set; }
+}
+
+
+public sealed record BuildError(
+    string Subcategory,
+    string Code,
+    string File,
+    int LineNumber,
+    int ColumnNumber,
+    int EndLineNumber,
+    int EndColumnNumber,
+    string? Message,
+    string? HelpKeyword,
+    string? SenderName);
