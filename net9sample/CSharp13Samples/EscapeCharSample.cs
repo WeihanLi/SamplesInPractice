8835f5c17124228a8d6aef06cda76e2c44f227d3
Weihan Li <weihanli@outlook.com>
2024-09-21 15:28:03 +0800

    add net9sample/CSharp13Samples/EscapeCharSample.cs

---
+++ net9sample/CSharp13Samples/EscapeCharSample.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharp13Samples;
+
+/// <summary>
+/// \e as a shortcut/short-hand replacement for the character code point 0x1b, commonly known as the ESCAPE (or ESC) character.
+/// https://github.com/dotnet/csharplang/blob/main/proposals/csharp-13.0/esc-escape-sequence.md
+/// </summary>
+public static class EscapeCharSample
+{
+    public static void MainTest()
+    {
+        var escapeChar = '\e';
+        var unicodeEscapeChar = '\u001b';
+
+        Console.WriteLine(escapeChar == unicodeEscapeChar);
+
+        Console.WriteLine((int)escapeChar);
+        Console.WriteLine((int)unicodeEscapeChar);
+    }
+}
