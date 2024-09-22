c00880b72776eb9cdde30d2df1b2deef1c1306d3
Weihan Li <weihanli@outlook.com>
2024-09-22 17:42:56 +0800

    fix net9sample/CSharp13Samples/EscapeCharSample comment warning

--- net9sample/CSharp13Samples/EscapeCharSample.cs
+++ net9sample/CSharp13Samples/EscapeCharSample.cs
@@ -1,10 +1,10 @@
 ﻿namespace CSharp13Samples;
 
+// Grep usage: https://grep.app/search?q=%5Cu001b&filter[lang][0]=C%23
+// GitHub Spectre Console SearchResult: https://github.com/search?q=repo%3Aspectreconsole%2Fspectre.console+%5Cu001b&type=code&p=3
 /// <summary>
 /// \e as a shortcut/short-hand replacement for the character code point 0x1b, commonly known as the ESCAPE (or ESC) character.
 /// https://github.com/dotnet/csharplang/blob/main/proposals/csharp-13.0/esc-escape-sequence.md
-/// <seealso href="https://grep.app/search?q=%5Cu001b&filter[lang][0]=C%23">grep search result</seealso>
-/// <seealso href="https://github.com/search?q=repo%3Aspectreconsole%2Fspectre.console+%5Cu001b&type=code&p=3">Github SpectreConsole SearchResult</seealso>
 /// </summary>
 public static class EscapeCharSample
 {
