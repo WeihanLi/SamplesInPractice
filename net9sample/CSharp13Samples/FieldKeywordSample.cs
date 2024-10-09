836f4445fcf19aaf2627807a87b05ae7968c8818
Weihan Li <weihanli@outlook.com>
2024-10-10 07:19:53 +0800

    update net9sample/CSharp13Samples
    add FieldKeywordSample.cs & RefUnsafeExtensionSample.cs

---
+++ net9sample/CSharp13Samples/FieldKeywordSample.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.Versioning;
+
+namespace CSharp13Samples;
+
+[RequiresPreviewFeatures]
+internal static class FieldKeywordSample
+{
+    // public static string Name { get; set => value = field?.Trim(); }
+}
---
+++ net9sample/CSharp13Samples/RefUnsafeExtensionSample.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharp13Samples;
+
+internal static class RefUnsafeExtensionSample
+{
+    public static async Task MainTest()
+    {
+        // ref in async method
+        var age = 10;
+        ref int i = age;
+        await Task.CompletedTask;
+    }
+
+
+    private static unsafe System.Collections.Generic.IEnumerable<int> M() // an iterator
+    {
+        yield return 1;
+        local();
+
+        async void local0()
+        {
+            //int* p = null; // allowed in C# 12; error in C# 13 (breaking change, unsafe context needed)        
+
+            await Task.Yield(); // error in C# 12, allowed in C# 13
+        }
+
+        async void local()
+        {
+            unsafe
+            {
+                int* p = null; // allowed in C# 12; error in C# 13 (breaking change, unsafe context needed)
+            }
+
+            await Task.Yield(); // error in C# 12, allowed in C# 13
+        }
+    }
+}
