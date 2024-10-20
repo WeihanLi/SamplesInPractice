2a6f47f221e6e26b8e0a576a40d2c49fdea3aaf9
Weihan Li <weihanli@outlook.com>
2024-10-20 22:45:28 +0800

    update FieldKeywordSample

--- net9sample/CSharp13Samples/FieldKeywordSample.cs
+++ net9sample/CSharp13Samples/FieldKeywordSample.cs
@@ -1,18 +1,42 @@
 ﻿namespace CSharp13Samples;
 
+// https://github.com/dotnet/csharplang/issues/140
+// https://github.com/dotnet/csharplang/blob/main/proposals/field-keyword.md
 internal static class FieldKeywordSample
 {
     public static string Name
-    { 
+    {
         get;
-        // set => field = value?.Trim() ?? string.Empty;
-        set;
+        set => field = value?.Trim() ?? string.Empty;
+        //set;
     } = string.Empty;
 
-    public static void MainTest()
+    public static string Description
+    {
+        get => field ?? string.Empty;
+        set
+        {
+            Console.Write(">>>");
+            Console.WriteLine(field);
+            Console.WriteLine($"Before set description, {nameof(value)}: {value}");
+            field = value?.Trim()!;
+            // CS0103: The name 'field' does not exist in the current context
+            // Console.WriteLine($"After set description, {field}");
+            Console.WriteLine("After set description");
+            Console.Write(">>>");
+            Console.WriteLine(field);
+        }
+    } = default!;
+
+    public static void Run()
     {
         Name = " Hello ";
         Console.WriteLine(Name);
         Console.WriteLine(Name.Length);
+
+        Console.WriteLine(Description is not null);
+        Description = " World ";
+        Console.WriteLine(Description);
+        Console.WriteLine(Description.Length);
     }
 }
--- net9sample/CSharp13Samples/Program.cs
+++ net9sample/CSharp13Samples/Program.cs
@@ -1,6 +1,7 @@
 ﻿//ImplicitIndexAccessSample.MainTest();
 //PartialPropertySample.MainTest();
 //RefStructInterfaceSample.MainTest();
-OverloadResolutionPrioritySample.MainTest();
+//OverloadResolutionPrioritySample.MainTest();
+//FieldKeywordSample.Run();
 
 Console.WriteLine("Completed!");
