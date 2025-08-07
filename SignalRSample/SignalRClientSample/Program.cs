ed5cfeb9c21e01a5e3fbb706982aff0edf80407a
Weihan Li <weihanli@outlook.com>
2025-08-07 08:28:54 +0800

    update SignalRSample

--- SignalRSample/SignalRClientSample/Program.cs
+++ SignalRSample/SignalRClientSample/Program.cs
@@ -1,25 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
 using SignalRSample;
 using System.Net;
 using System.Text.Json;
 using TypedSignalR.Client;
 
 await using var connection = new HubConnectionBuilder()
+    .ConfigureLogging(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Debug).AddDefaultDelegateLogger())
     // .WithUrl("https://localhost:5002/hub/hello", options =>
+    // .WithUrl("http://localhost:5001/hub/hello", options =>
     .WithUrl("http://localhost:5000/hub/hello", options =>
     {
         options.SkipNegotiation = true;
         options.Transports = HttpTransportType.WebSockets;
+        options.HttpMessageHandlerFactory = _ => new SocketsHttpHandler();
         options.WebSocketConfiguration = clientWebSocketOptions =>
         {
-            // clientWebSocketOptions.Proxy = new WebProxy("http://localhost:8000");
+            clientWebSocketOptions.Proxy = new WebProxy("http://localhost:8000");
             // clientWebSocketOptions.HttpVersion = HttpVersion.Version20;
-            // clientWebSocketOptions.HttpVersionPolicy = HttpVersionPolicy.RequestVersionExact;
+            // clientWebSocketOptions.HttpVersionPolicy = HttpVersionPolicy.RequestVersionOrLower;
         };
     })
     .Build();
-
 connection.Reconnected += c =>
 {
     Console.WriteLine($"reconnected, connectionId: {c}");
