16007d2eb7190009e1d5d22f6bea36e911c2380d
Weihan Li <weihanli@outlook.com>
2026-03-17 08:46:36 +0800

    feat: add Net11Samples

---
+++ net11sample/CSharp15Samples/CollectionExpressionPlus.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp15Samples;
+
+internal static class CollectionExpressionPlus
+{
+    public static void Run()
+    {
+        List<int> list = [1, 2, 3];
+        HashSet<int> set = [1, 2, 3];
+        HashSet<string> set2 = ["one", "two", "three"];
+        HashSet<string> set3 = [with(StringComparer.OrdinalIgnoreCase), "one", "two", "three", "One"];
+        Console.WriteLine("List: " + string.Join(", ", list));
+        Console.WriteLine("Set: " + string.Join(", ", set));
+        Console.WriteLine("Set2: " + string.Join(", ", set2));
+        Console.WriteLine("Set3: " + string.Join(", ", set3));
+    }
+}
---
+++ net11sample/CSharp15Samples/Program.cs
@@ -0,0 +1,4 @@
+﻿using CSharp15Samples;
+
+Console.WriteLine("Hello C# 15!");
+CollectionExpressionPlus.Run();
