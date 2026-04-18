d5f6cd4fbba3522c511c7501777c33b114115e7f
Weihan Li <weihanli@outlook.com>
2026-04-18 21:52:55 +0800

    update net11sample/file-scripts sample

--- net11sample/file-scripts/hello.cs
+++ net11sample/file-scripts/hello.cs
@@ -1,4 +1,6 @@
 ﻿#!/usr/bin/env dotnet
 #:include helper.cs
 
+Helper.WriteToConsole("Hello, World!");
+
 Helper.Print("Hello, World!");
--- net11sample/file-scripts/helper.cs
+++ net11sample/file-scripts/helper.cs
@@ -1,8 +1,13 @@
-﻿#:package WeihanLi.Common@1.0.87
+﻿#:package WeihanLi.Common@1.0.88
 using static WeihanLi.Common.Helpers.ConsoleHelper;
 
 public static class Helper
 {
+    public static void WriteToConsole(string message)
+    {
+        Console.WriteLine(message);
+    }
+
     public static void Print(string message)
     {
         WriteLineWithColor(message, ConsoleColor.Green);
